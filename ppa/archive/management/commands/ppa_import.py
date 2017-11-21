1b1121cc29ece202c71c71e0dc1abf4382986705
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-11-21 16:00:15 -0500

    Revise import script to use bibliographic api for metadata #4

--- ppa/archive/management/commands/ppa_import.py
+++ ppa/archive/management/commands/ppa_import.py
@@ -6,8 +6,8 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 from SolrClient import SolrClient
 from pairtree import pairtree_path, pairtree_client
-import pandas
 
+from ppa.archive.hathi import HathiBibliographicAPI
 from ppa.archive.models import DigitizedWork
 
 
@@ -31,11 +31,7 @@ class Command(BaseCommand):
 
         self.solr = SolrClient(solr_config['URL'])
         self.solr_collection = solr_config['COLLECTION']
-
-        # hathifile_metadata = read_table(settings.HATHIFILES,
-        #     header=None, index_col=0)
-        # print(hathifile_metadata)
-        # print(hathifile_metadata.index)
+        bib_api = HathiBibliographicAPI()
 
         # bulk import only for now
         # - eventually support list of ids + rsync?
@@ -45,25 +41,18 @@ class Command(BaseCommand):
 
         for htid in self.get_hathi_ids():
             print(htid)
-            # try:
-            #     print(hathifile_metadata.loc[htid])
-            # except:
-            #     print('%s not in hathifile' % htid)
-
             prefix, pt_id = htid.split('.', 1)
-            print('ptid = %s' % pt_id)
             # pairtree id to path for data files
             ptobj = self.hathi_pairtree[prefix].get_object(pt_id,
                 create_if_doesnt_exist=False)
-            print(ptobj.id_to_dirpath())
             # contents are stored in a directory named based on a
             # pairtree encoded version of the id
             content_dir = pairtree_path.id_encode(pt_id)
             # - expect a mets file and a zip file
             ht_metsfile, ht_zipfile = ptobj.list_parts(content_dir)
             # print(ptobj.list_parts(pairtree_path.id_encode(pt_id)))
-            print(ht_zipfile)
             solr_docs = []
+            # read zipfile contents in place, without unzipping
             with ZipFile(os.path.join(ptobj.id_to_dirpath(), content_dir, ht_zipfile)) as ht_zip:
                 filenames = ht_zip.namelist()
                 page_count = len(filenames)
@@ -81,55 +70,32 @@ class Command(BaseCommand):
 
             # create stub database record
             digwork, created = DigitizedWork.objects.get_or_create(source_id=htid)
+
+            # get brief bibliographic record from hathi bib api
+            bibdata = bib_api.brief_record('htid', htid)
+            if bibdata:
+                digwork.title = bibdata.title
+                # NOTE: may also include sort title
+                # pub date is list; just use first for now
+                digwork.pub_date = bibdata.pub_dates[0]
+                # digwork.enumcron = bibdata.pub_dates[0]
+                copy_details = bibdata.copy_details(htid)
+                digwork.enumcron = copy_details['enumcron'] or ''
+                # TODO: should also consider storing:
+                # - last update, rights code / rights string, item url
+                # (maybe solr only?)
+
             digwork.page_count = page_count
             # TODO: only save if changed (so updated time will be accurate)
             digwork.save()
 
-            idx = self.solr.index(self.solr_collection, [{'id': htid, 'item_type': 'work'}])
-            print(idx)
+            # update work details in solr
+            # TODO: solrdoc method on model
+            solr_doc = {'id': htid, 'item_type': 'work', 'title': digwork.title,
+                        'pub_date': digwork.pub_date, 'enumcron': digwork.enumcron}
+            idx = self.solr.index(self.solr_collection, [solr_doc])
 
         self.solr.commit(self.solr_collection)
-
-        # get bibliographic metadata
-        # json api
-        # max 20 records at a time; full or brief record
-        # syntax for multiple is htid:id.1|htid:id.2|htid:id.3
-    # https://catalog.hathitrust.org/api/volumes/full/json/htid:njp.32101013082597%7Chtid:hvd.32044011432754%7Chtid:chi.085137191
-
-        count = 0
-        with open(settings.HATHIFILES) as hathimetadata:
-            for line in hathimetadata:
-                count += 1
-                meta_id, data = line.split('\t', 1)
-                # print("metaid %s" % meta_id)
-                results = self.solr.query(self.solr_collection, {
-                    'q': 'item_type:work AND id:"%s"' % meta_id,
-                    'fl': 'id',
-                    # 'rows': 0
-                })
-                if results.get_results_count():
-                    print('found match for %s' % meta_id)
-                    item_info = data.split('\t')
-                    print(item_info[10]) # title
-                    print(item_info[15]) # pub date
-                    print(item_info[16]) # pub place
-
-                    solr_doc = {'id': meta_id, 'item_type': 'work', 'title': item_info[10],
-                        'pub_date': item_info[15]}
-                    print(solr_doc)
-                    idx = self.solr.index(self.solr_collection, [solr_doc])
-
-                if count % 1000 == 0:
-                    print(count)
-
-
-            print(count)
-            # - create stub record
-            # - add metadata to solr
-            # get pages
-            # - add to solr
-
-            self.solr.commit(self.solr_collection)
 
     def get_hathi_ids(self):
         # generator of hathi ids from previously rsynced hathitrust data
@@ -142,16 +108,19 @@ class Command(BaseCommand):
             # rsync data provided by hathi doesn't include version file,
             # but according to python pairtree package that's invalid'
             # - create version file if it doesn't exist
-            ptree_version = os.path.join(ht_data_dir, 'pairtree_version0_1')
-            if not os.path.exists(ptree_version):
-                with open(ptree_version, 'w'):
-                    pass
+            # ptree_version = os.path.join(ht_data_dir, 'pairtree_version0_1')
+            # if not os.path.exists(ptree_version):
+            #     with open(ptree_version, 'w'):
+            #         pass
 
             # maybe store the pairtree clients by prefix?
             hathi_ptree = pairtree_client.PairtreeStorageClient(prefix, ht_data_dir)
             # store initialized pairtree for later use
             self.hathi_pairtree[prefix] = hathi_ptree
             for hathi_id in hathi_ptree.list_ids():
+                # NOTE: prefix should automatially be handled based on
+                # pairtree_prefix, but python pairtree library doesn't
+                # yet include logic for that
                 yield '%s.%s' % (prefix, hathi_id)
 
 
