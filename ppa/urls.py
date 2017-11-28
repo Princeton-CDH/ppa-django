46b088ef456bffe5726dbce69fc4e3dc822a45fa
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-11-28 17:26:05 -0500

    Add mezzanine and harmonize with grappelli

--- ppa/urls.py
+++ ppa/urls.py
@@ -16,15 +16,19 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic.base import TemplateView
+import mezzanine.urls
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
     # grappelli URLS for admin related lookups & autocompletes
     url(r'^grappelli/', include('grappelli.urls')),
     # pucas urls for CAS login
     url(r'^accounts/', include('pucas.cas_urls')),
     # placeholder for home page
-    url(r'^$', TemplateView.as_view(template_name='site_index.html'), name='site-index'),
+    url(r'^$', TemplateView.as_view(template_name='site_index.html'), name='home'),
     url(r'^archive/', include('ppa.archive.urls', namespace='archive')),
+
+    # content pages managed by mezzanine
+    url("^", include(mezzanine.urls))
 ]
