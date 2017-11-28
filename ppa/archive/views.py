ccf60c26874c2cbfcdc9eea7bd36ce9414d41eae
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-11-28 15:41:42 -0500

    Add & display search form to make keyword search testable #6
    Also improves solr join query; now handles exact phrases

--- ppa/archive/views.py
+++ ppa/archive/views.py
@@ -1,8 +1,14 @@
 import json
+import logging
 
 from django.conf import settings
 from django.views.generic import TemplateView
 from SolrClient import SolrClient
+
+from ppa.archive.forms import SearchForm
+
+
+logger = logging.getLogger(__name__)
 
 
 class ItemListView(TemplateView):
@@ -19,26 +25,31 @@ class ItemListView(TemplateView):
         solr = SolrClient(solr_config['URL'])
         solr_collection = solr_config['COLLECTION']
 
-        query = self.request.GET.get("query", "")
+        form = SearchForm(self.request.GET)
+        query = join_q = None
+        if form.is_valid():
+            query = form.cleaned_data.get("query", "")
         if query:
-            # simple version
-            solr_q = "text:(%s)" % query
+            # simple keyword search across all text content
+            solr_q = join_q = "text:(%s)" % query
             # use join to ensure we always get the work if any pages match
             # FIXME: solr join query errors on () or "" to group multiple terms
-            solr_q = 'text:"%s" OR {!join from=htid to=id}text:%s' % (query, query)
+            # solr_q = 'text:"%s" OR {!join from=htid to=id}text:%s' % (query, query)
+            solr_q = 'text:(%s) OR {!join from=htid to=id v=$join_query}' % (query)
         else:
             # no search term - find everything
             solr_q = "*:*"
-
-        print(solr_q)
+        logger.debug("Solr search query: %s", solr_q)
 
         response = solr.query(solr_collection, {
             'q': solr_q,
-            # 'q': '*:*',
             'fq': '{!collapse field=htid sort="item_type desc"}',
-            'expand': 'true'
+            'expand': 'true',
+            'join_query': join_q,
+            'rows': 50  # override solr default of 10 results; display 50 at a time for now
         })
         context.update({
+            'search_form': form,
             'total': response.get_num_found(),
             'items': response.docs,
             'page_groups': json.loads(response.get_json())['expanded']
