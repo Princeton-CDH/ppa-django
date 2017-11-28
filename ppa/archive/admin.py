d629f55e29ff80a712e93d09914fc56f7b37ecda
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-11-28 15:15:08 -0500

    Configure search fields for digitized work admin view #5

--- ppa/archive/admin.py
+++ ppa/archive/admin.py
@@ -7,6 +7,10 @@ class DigitizedWorkAdmin(admin.ModelAdmin):
     list_display = ('source_id', 'title', 'author', 'enumcron', 'pub_place',
         'pub_date', 'page_count', 'added', 'updated')
     readonly_fields = ('source_id', 'page_count', 'added', 'updated')
+    search_fields = ('source_id', 'title', 'author', 'enumcron', 'pub_date')
+    # date_hierarchy = 'added'  # is this useful?
+    # currently nothing useful to filter on; eventually will have collectio
+    # list_filter = []
 
 
 admin.site.register(DigitizedWork, DigitizedWorkAdmin)
