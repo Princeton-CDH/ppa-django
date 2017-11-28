d44499f342d3d7cd959c496e4a60d5070b3e4da7
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-11-28 16:08:17 -0500

    Basic pytest configuration and initial test for hathi bibdata #4

--- ppa/archive/tests.py
+++ ppa/archive/tests.py
@@ -1,3 +1,31 @@
+import json
+import os.path
+
+from django.conf import settings
 from django.test import TestCase
 
-# Create your tests here.
+from ppa.archive.hathi import HathiBibliographicRecord
+
+FIXTURES_PATH = os.path.join(settings.BASE_DIR, 'ppa', 'archive', 'fixtures')
+
+class TestHathiBibliographicRecord(TestCase):
+    bibdata_fixture = os.path.join(FIXTURES_PATH,
+        'bibdata_full_njp.32101013082597.json')
+
+    def setUp(self):
+        with open(self.bibdata_fixture) as bibdata:
+            self.record = HathiBibliographicRecord(json.load(bibdata))
+
+    def test_properties(self):
+        record = self.record
+        assert record.record_id == '008883512'
+        assert record.title == \
+            "Lectures on the literature of the age of Elizabeth, and Characters of Shakespear's plays,"
+        assert record.pub_dates == ['1882']
+        copy_details = record.copy_details('njp.32101013082597')
+        assert isinstance(copy_details, dict)
+        assert copy_details['orig'] == 'Princeton University'
+
+        assert record.copy_details('bogus') is None
+
+
