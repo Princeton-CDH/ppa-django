213b2f01b1d0c7d765adf7670b9aca3f7f0d33c6
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-11-28 16:16:01 -0500

    Initial travis-ci build

---
+++ ci/testsettings.py
@@ -0,0 +1,25 @@
+# This file is exec'd from settings.py, so it has access to and can
+# modify all the variables in settings.py.
+
+# If this file is changed in development, the development server will
+# have to be manually restarted because changes will not be noticed
+# immediately.
+
+DEBUG = False
+
+# include database settings to use Mariadb ver on production (5.5)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
+        'TEST': {
+                'CHARSET': 'utf8',
+                'COLLATION': 'utf8_general_ci',
+            },
+    },
+}
+
+# secret key added as a travis build step
