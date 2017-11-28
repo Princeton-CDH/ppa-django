745ac9f3e15319df57e01ab2d83b03b1295530bb
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-11-28 16:41:34 -0500

    Add logic and tests for handling marcxml in hathi bibdata

--- ppa/archive/tests.py
+++ ppa/archive/tests.py
@@ -3,18 +3,24 @@ import os.path
 
 from django.conf import settings
 from django.test import TestCase
+import pymarc
 
 from ppa.archive.hathi import HathiBibliographicRecord
 
 FIXTURES_PATH = os.path.join(settings.BASE_DIR, 'ppa', 'archive', 'fixtures')
 
 class TestHathiBibliographicRecord(TestCase):
-    bibdata_fixture = os.path.join(FIXTURES_PATH,
+    bibdata_full = os.path.join(FIXTURES_PATH,
         'bibdata_full_njp.32101013082597.json')
+    bibdata_brief = os.path.join(FIXTURES_PATH,
+        'bibdata_brief_njp.32101013082597.json')
 
     def setUp(self):
-        with open(self.bibdata_fixture) as bibdata:
+        with open(self.bibdata_full) as bibdata:
             self.record = HathiBibliographicRecord(json.load(bibdata))
+
+        with open(self.bibdata_brief) as bibdata:
+            self.brief_record = HathiBibliographicRecord(json.load(bibdata))
 
     def test_properties(self):
         record = self.record
@@ -27,5 +33,26 @@ class TestHathiBibliographicRecord(TestCase):
         assert copy_details['orig'] == 'Princeton University'
 
         assert record.copy_details('bogus') is None
+
+        # brief record should work the same way
+        record = self.brief_record
+        assert record.record_id == '008883512'
+        assert record.title == \
+            "Lectures on the literature of the age of Elizabeth, and Characters of Shakespear's plays,"
+        assert record.pub_dates == ['1882']
+        copy_details = record.copy_details('njp.32101013082597')
+        assert isinstance(copy_details, dict)
+        assert copy_details['orig'] == 'Princeton University'
+
+        assert record.copy_details('bogus') is None
+
+    def test_marcxml(self):
+        record = self.record
+        assert isinstance(record.marcxml, pymarc.Record)
+        assert record.marcxml.author() == 'Hazlitt, William, 1778-1830.'
+
+        # test no marcxml in data, e.g. brief record
+        assert self.brief_record.marcxml is None
+
 
 
