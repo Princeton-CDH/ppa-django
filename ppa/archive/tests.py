8c9979ec0be5f832dd8496be2e88dbd7fc3f6f79
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-12-05 17:44:58 -0500

    Unit tests for digwork - populate from hathi, solr index data #4

--- ppa/archive/tests.py
+++ ppa/archive/tests.py
@@ -9,6 +9,7 @@ import requests
 from SolrClient import SolrClient
 
 from ppa.archive.hathi import HathiBibliographicRecord
+from ppa.archive.models import DigitizedWork
 from ppa.archive.solr import get_solr_connection, SolrSchema, CoreAdmin
 
 
@@ -59,7 +60,6 @@ class TestHathiBibliographicRecord(TestCase):
 
         # test no marcxml in data, e.g. brief record
         assert self.brief_record.marcxml is None
-
 
 
 TEST_SOLR_CONNECTIONS = {
@@ -163,5 +163,62 @@ class TestCoreAdmin(TestCase):
         # failure
         mockrequests.get.return_value.status_code = requests.codes.not_found
         assert not core_adm.reload('othercore')
+
+
+class TestDigitizedWork(TestCase):
+
+    def test_populate_from_bibdata(self):
+        with open(TestHathiBibliographicRecord.bibdata_full) as bibdata:
+            full_bibdata = HathiBibliographicRecord(json.load(bibdata))
+
+        with open(TestHathiBibliographicRecord.bibdata_brief) as bibdata:
+            brief_bibdata = HathiBibliographicRecord(json.load(bibdata))
+
+        digwork = DigitizedWork(source_id='njp.32101013082597')
+        digwork.populate_from_bibdata(brief_bibdata)
+        assert digwork.title == brief_bibdata.title
+        assert digwork.pub_date == brief_bibdata.pub_dates[0]
+        # no enumcron in this record
+        assert digwork.enumcron == ''
+        # fields from marc not set
+        assert not digwork.author
+        assert not digwork.pub_place
+        assert not digwork.publisher
+
+        # test no pub date
+        brief_bibdata.info['publishDates'] = []
+        digwork = DigitizedWork(source_id='njp.32101013082597')
+        digwork.populate_from_bibdata(brief_bibdata)
+        assert not digwork.pub_date
+
+        # TODO: test enumcron from copy details
+
+        # populate from full record
+        digwork.populate_from_bibdata(full_bibdata)
+        assert digwork.author == full_bibdata.marcxml.author()
+        assert digwork.pub_place == full_bibdata.marcxml['260']['a']
+        assert digwork.publisher == full_bibdata.marcxml['260']['b']
+
+        # TODO: test publication info unavailable?
+
+    def test_index_data(self):
+        digwork = DigitizedWork(source_id='njp.32101013082597',
+            title='Structure of English Verse', pub_date=1884,
+            author='Charles Witcomb', pub_place='Paris',
+            publisher='Mesnil-Dramard')
+        index_data = digwork.index_data()
+        assert index_data['id'] == digwork.source_id
+        assert index_data['htid'] == digwork.source_id
+        assert index_data['item_type'] == 'work'
+        assert index_data['title'] == digwork.title
+        assert index_data['author'] == digwork.author
+        assert index_data['pub_place'] == digwork.pub_place
+        assert index_data['pub_date'] == digwork.pub_date
+        assert index_data['publisher'] == digwork.publisher
+        assert not index_data['enumcron']
+
+        # with enumcron
+        digwork.enumcron = 'v.7 (1848)'
+        assert digwork.index_data()['enumcron'] == digwork.enumcron
 
 
