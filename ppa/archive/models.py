d887c3979cef1274bcd54ed5c191a5a0b76e5f7d
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-12-05 17:10:41 -0500

    Revise digitized work model: add publisher, import/index logic #4

--- ppa/archive/models.py
+++ ppa/archive/models.py
@@ -10,8 +10,8 @@ class DigitizedWork(models.Model):
     # original id / url?
     #: source identifier; hathi id for HathiTrust materials
     source_id = models.CharField(max_length=255, unique=True)
-    #: title of the work
-    title = models.CharField(max_length=255)
+    #: title of the work; using TextField to allow for long titles
+    title = models.TextField()
     #: enumeration/chronology
     enumcron = models.CharField('Enumeration/Chronology', max_length=255,
         null=True)
@@ -19,7 +19,10 @@ class DigitizedWork(models.Model):
 
     # TODO: foreign key for author?
     author = models.CharField(max_length=255)
+    #: place of publication
     pub_place = models.CharField('Place of Publication', max_length=255)
+    #: publisher
+    publisher = models.CharField(max_length=255)
     # using char for now in case not all numeric
     pub_date = models.CharField('Publication Date', max_length=255)
     #: number of pages in the work
@@ -28,6 +31,47 @@ class DigitizedWork(models.Model):
     added = models.DateTimeField(auto_now_add=True)
     #: date of last modification of the local record
     updated = models.DateTimeField(auto_now=True)
+
+    def populate_from_bibdata(self, bibdata):
+        '''Update record fields based on Hathi bibdata information.
+        Full record is required in order to set all fields
+
+        :param bibdata: bibliographic data returned from HathiTrust
+            as instance of :class:`ppa.archive.hathi.HathiBibliographicRecord`
+
+        '''
+        self.title = bibdata.title
+        # NOTE: might also want to store sort title
+        # pub date returned in api JSOn is list; use first for now (if available)
+        if bibdata.pub_dates:
+            self.pub_date = bibdata.pub_dates[0]
+        copy_details = bibdata.copy_details(self.source_id)
+        # hathi version/volume information for this specificy copy of a work
+        self.enumcron = copy_details['enumcron'] or ''
+        # set fields from marc if available
+        if bibdata.marcxml:
+            self.author = bibdata.marcxml.author() or ''
+            if '260' in bibdata.marcxml:
+                self.pub_place = bibdata.marcxml['260']['a']
+                self.publisher = bibdata.marcxml['260']['b']
+            # maybe: consider getting volume & series directly from
+            # marc rather than relying on hathi enumcron ()
+
+        # should also consider storing:
+        # - last update, rights code / rights string, item url
+        # (maybe solr only?)
+
+    def index_data(self):
+        '''data for indexing in Solr'''
+        return {
+            'id': self.source_id,
+            'htid': self.source_id,
+            'item_type': 'work',
+            'title': self.title,
+            'pub_date': self.pub_date,
+            'enumcron': self.enumcron,
+            'author': self.author
+        }
 
 
 
