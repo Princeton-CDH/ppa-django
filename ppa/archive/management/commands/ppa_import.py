f72fa58e5f3720fa7b3913a17f0d17a0be3a83f3
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-12-05 17:13:23 -0500

    Refine import script based on solr & digwork model refactors #4

--- ppa/archive/management/commands/ppa_import.py
+++ ppa/archive/management/commands/ppa_import.py
@@ -9,6 +9,7 @@ from pairtree import pairtree_path, pairtree_client
 
 from ppa.archive.hathi import HathiBibliographicAPI
 from ppa.archive.models import DigitizedWork
+from ppa.archive.solr import get_solr_connection
 
 
 class Command(BaseCommand):
@@ -20,11 +21,7 @@ class Command(BaseCommand):
     hathi_pairtree = {}
 
     def handle(self, *args, **kwargs):
-        # TODO: error handling etc
-        solr_config = settings.SOLR_CONNECTIONS['default']
-
-        self.solr = SolrClient(solr_config['URL'])
-        self.solr_collection = solr_config['COLLECTION']
+        self.solr, self.solr_collection = get_solr_connection()
         bib_api = HathiBibliographicAPI()
 
         # bulk import only for now
@@ -32,9 +29,10 @@ class Command(BaseCommand):
         # for now, start with existing rsync data
         # - get list of ids, rsync data, grab metadata
         # - populate db and solr (should add/update if already exists)
-
+        total = self.count_hathi_ids()
+        self.stdout.write('Found %d items to import' % total)
         for htid in self.get_hathi_ids():
-            print(htid)
+            self.stdout.write(htid)
             prefix, pt_id = htid.split('.', 1)
             # pairtree id to path for data files
             ptobj = self.hathi_pairtree[prefix].get_object(pt_id,
@@ -59,39 +57,23 @@ class Command(BaseCommand):
                            'content': pagefile.read().decode('utf-8'),
                            'item_type': 'page'
                         })
-                # print(len(ht_zip.namelist()))
-                idx = self.solr.index(self.solr_collection, solr_docs)
+                self.solr.index(self.solr_collection, solr_docs)
 
             # create stub database record
             digwork, created = DigitizedWork.objects.get_or_create(source_id=htid)
 
             # get brief bibliographic record from hathi bib api
+            # TODO: maybe only if created?
             bibdata = bib_api.record('htid', htid)
             if bibdata:
-                digwork.title = bibdata.title
-                # NOTE: may also include sort title
-                # pub date is list; just use first for now (if available)
-                if bibdata.pub_dates:
-                    digwork.pub_date = bibdata.pub_dates[0]
-                copy_details = bibdata.copy_details(htid)
-                digwork.enumcron = copy_details['enumcron'] or ''
-                if bibdata.marcxml:
-                    digwork.author = bibdata.marcxml.author() or ''
-
-                # TODO: should also consider storing:
-                # - last update, rights code / rights string, item url
-                # (maybe solr only?)
+                digwork.populate_from_bibdata(bibdata)
 
             digwork.page_count = page_count
             # TODO: only save if changed (so updated time will be accurate)
             digwork.save()
 
             # update work details in solr
-            # TODO: solrdoc method on model
-            solr_doc = {'id': htid, 'htid': htid, 'item_type': 'work', 'title': digwork.title,
-                        'pub_date': digwork.pub_date, 'enumcron': digwork.enumcron,
-                        'author': digwork.author}
-            idx = self.solr.index(self.solr_collection, [solr_doc])
+            self.solr.index(self.solr_collection, [digwork.index_data()])
 
         self.solr.commit(self.solr_collection)
 
@@ -103,22 +85,21 @@ class Command(BaseCommand):
         hathi_dirs = glob(os.path.join(settings.HATHI_DATA, '*'))
         for ht_data_dir in hathi_dirs:
             prefix = os.path.basename(ht_data_dir)
-            # rsync data provided by hathi doesn't include version file,
-            # but according to python pairtree package that's invalid'
-            # - create version file if it doesn't exist
-            # ptree_version = os.path.join(ht_data_dir, 'pairtree_version0_1')
-            # if not os.path.exists(ptree_version):
-            #     with open(ptree_version, 'w'):
-            #         pass
 
-            # maybe store the pairtree clients by prefix?
             hathi_ptree = pairtree_client.PairtreeStorageClient(prefix, ht_data_dir)
-            # store initialized pairtree for later use
+            # store initialized pairtree client by prefix for later use
             self.hathi_pairtree[prefix] = hathi_ptree
             for hathi_id in hathi_ptree.list_ids():
                 # NOTE: prefix should automatially be handled based on
                 # pairtree_prefix, but python pairtree library doesn't
                 # yet include logic for that
                 yield '%s.%s' % (prefix, hathi_id)
+
+    def count_hathi_ids(self):
+        # count items in the pairtree structure without loading
+        # all into memory at once
+        # NOTE: probably should still check how slow this is on
+        # the full dataset...
+        return sum(1 for i in self.get_hathi_ids())
 
 
