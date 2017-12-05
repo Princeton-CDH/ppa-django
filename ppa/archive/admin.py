f91d4fc2431d20c5ef68e3ef788957e149eec25b
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-12-05 17:10:41 -0500

    Revise digitized work model: add publisher, import/index logic #4

--- ppa/archive/admin.py
+++ ppa/archive/admin.py
@@ -5,7 +5,7 @@ from ppa.archive.models import DigitizedWork
 
 class DigitizedWorkAdmin(admin.ModelAdmin):
     list_display = ('source_id', 'title', 'author', 'enumcron', 'pub_place',
-        'pub_date', 'page_count', 'added', 'updated')
+        'publisher', 'pub_date', 'page_count', 'added', 'updated')
     readonly_fields = ('source_id', 'page_count', 'added', 'updated')
     search_fields = ('source_id', 'title', 'author', 'enumcron', 'pub_date')
     # date_hierarchy = 'added'  # is this useful?
