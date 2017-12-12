19265d7e22745f500104a66cea360b8afbb3810d
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-12-12 10:52:30 -0500

    Rename digitized work list view and template to be more specific

--- ppa/archive/urls.py
+++ ppa/archive/urls.py
@@ -4,6 +4,6 @@ from ppa.archive import views
 
 
 urlpatterns = [
-    url('^$', views.ItemListView.as_view(), name='list'),
+    url('^$', views.DigitizedWorkListView.as_view(), name='list'),
     url('^(?P<id>.+)/$', views.ItemDetailView.as_view(), name='detail'),
 ]
