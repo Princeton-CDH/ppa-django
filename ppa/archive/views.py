8791453adda5153b74febd0c19334333b994148f
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-12-12 17:12:18 -0500

    Preliminary pagination of solr search results #6

--- ppa/archive/views.py
+++ ppa/archive/views.py
@@ -1,35 +1,34 @@
 import json
 import logging
+import operator
 
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import ListView, DetailView
 from SolrClient import SolrClient
+from SolrClient import SolrResponse
 
 from ppa.archive.forms import SearchForm
 from ppa.archive.models import DigitizedWork
+from ppa.archive.solr import PagedSolrQuery
+
 
 logger = logging.getLogger(__name__)
 
 
-class DigitizedWorkListView(TemplateView):
+class DigitizedWorkListView(ListView):
 
     template_name = 'archive/list_digitizedworks.html'
     # NOTE: listview would be nice, but would have to figure out how to
     # make solrclient compatible with django pagination
 
-    def get_context_data(self, **kwargs):
-        context = super(DigitizedWorkListView, self).get_context_data(**kwargs)
-        # todo: utility method to init solr client
-        # - with maybe a wrapper that sets default
-        solr_config = settings.SOLR_CONNECTIONS['default']
-        solr = SolrClient(solr_config['URL'])
-        solr_collection = solr_config['COLLECTION']
+    paginate_by = 50
 
-        form = SearchForm(self.request.GET)
+    def get_queryset(self, **kwargs):
+        self.form = SearchForm(self.request.GET)
         query = join_q = None
-        if form.is_valid():
-            query = form.cleaned_data.get("query", "")
+        if self.form.is_valid():
+            query = self.form.cleaned_data.get("query", "")
         if query:
             # simple keyword search across all text content
             solr_q = join_q = "text:(%s)" % query
@@ -39,20 +38,20 @@ class DigitizedWorkListView(TemplateView):
             # to support exact phrase searches
             solr_q = 'text:(%s) OR {!join from=srcid to=id v=$join_query}' % (query)
             # sort by relevance, return score for display
-            sort = 'relevance'
+            self.sort = 'relevance'
             solr_sort = 'score desc'
             fields = '*,score'
         else:
             # no search term - find everything
             solr_q = "*:*"
             # for now, use title for default sort
-            sort = 'title'
+            self.sort = 'title'
             solr_sort = 'title_exact asc'
             fields = '*'
 
         logger.debug("Solr search query: %s", solr_q)
 
-        response = solr.query(solr_collection, {
+        self.solrq = PagedSolrQuery({
             'q': solr_q,
             'sort': solr_sort,
             'fl': fields,
@@ -62,14 +61,18 @@ class DigitizedWorkListView(TemplateView):
             'expand': 'true',
             'expand.rows': 10,   # number of items in the collapsed group, i.e pages to display
             'join_query': join_q,
-            'rows': 50  # override solr default of 10 results; display 50 at a time for now
+            # 'rows': 50  # override solr default of 10 results; display 50 at a time for now
         })
+        return self.solrq
+
+    def get_context_data(self, **kwargs):
+        context = super(DigitizedWorkListView, self).get_context_data(**kwargs)
+
         context.update({
-            'search_form': form,
-            'total': response.get_num_found(),
-            'items': response.docs,
-            'sort': sort,
-            'page_groups': json.loads(response.get_json())['expanded']
+            'search_form': self.form,
+            # total and object_list provided by paginator
+            'sort': self.sort,
+            'page_groups': json.loads(self.solrq.get_json())['expanded']
         })
         return context
 
@@ -82,4 +85,4 @@ class ItemDetailView(DetailView):
         '''Override get_object to use source_id as lookup criterion'''
         source_id = self.kwargs.get('id', None)
         return get_object_or_404(DigitizedWork, source_id=source_id)
-    
+
