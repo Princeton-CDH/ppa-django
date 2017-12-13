5946d444c40f36f9c4427254b9119513a7a505a5
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2017-12-13 11:53:24 -0500

    Add DetailView for DigitizedWork + url #7

--- ppa/archive/views.py
+++ ppa/archive/views.py
@@ -1,33 +1,35 @@
 import json
 import logging
-import operator
 
 from django.conf import settings
-from django.views.generic import ListView
+from django.shortcuts import get_object_or_404
+from django.views.generic import TemplateView, DetailView
 from SolrClient import SolrClient
-from SolrClient import SolrResponse
 
 from ppa.archive.forms import SearchForm
-from ppa.archive.solr import PagedSolrQuery
-
+from ppa.archive.models import DigitizedWork
 
 logger = logging.getLogger(__name__)
 
 
+class ItemListView(TemplateView):
 
-class DigitizedWorkListView(ListView):
-
-    template_name = 'archive/list_digitizedworks.html'
+    template_name = 'archive/list_items.html'
     # NOTE: listview would be nice, but would have to figure out how to
     # make solrclient compatible with django pagination
 
-    paginate_by = 50
+    def get_context_data(self, **kwargs):
+        context = super(ItemListView, self).get_context_data(**kwargs)
+        # todo: utility method to init solr client
+        # - with maybe a wrapper that sets default
+        solr_config = settings.SOLR_CONNECTIONS['default']
+        solr = SolrClient(solr_config['URL'])
+        solr_collection = solr_config['COLLECTION']
 
-    def get_queryset(self, **kwargs):
-        self.form = SearchForm(self.request.GET)
+        form = SearchForm(self.request.GET)
         query = join_q = None
-        if self.form.is_valid():
-            query = self.form.cleaned_data.get("query", "")
+        if form.is_valid():
+            query = form.cleaned_data.get("query", "")
         if query:
             # simple keyword search across all text content
             solr_q = join_q = "text:(%s)" % query
@@ -37,20 +39,20 @@ class DigitizedWorkListView(ListView):
             # to support exact phrase searches
             solr_q = 'text:(%s) OR {!join from=srcid to=id v=$join_query}' % (query)
             # sort by relevance, return score for display
-            self.sort = 'relevance'
+            sort = 'relevance'
             solr_sort = 'score desc'
             fields = '*,score'
         else:
             # no search term - find everything
             solr_q = "*:*"
             # for now, use title for default sort
-            self.sort = 'title'
+            sort = 'title'
             solr_sort = 'title_exact asc'
             fields = '*'
 
         logger.debug("Solr search query: %s", solr_q)
 
-        self.solrq = PagedSolrQuery({
+        response = solr.query(solr_collection, {
             'q': solr_q,
             'sort': solr_sort,
             'fl': fields,
@@ -60,19 +62,24 @@ class DigitizedWorkListView(ListView):
             'expand': 'true',
             'expand.rows': 10,   # number of items in the collapsed group, i.e pages to display
             'join_query': join_q,
-            # 'rows': 50  # override solr default of 10 results; display 50 at a time for now
+            'rows': 50  # override solr default of 10 results; display 50 at a time for now
         })
-        return self.solrq
-
-    def get_context_data(self, **kwargs):
-        context = super(DigitizedWorkListView, self).get_context_data(**kwargs)
-
         context.update({
-            'search_form': self.form,
-            # total and object_list provided by paginator
-            'sort': self.sort,
-            'page_groups': json.loads(self.solrq.get_json())['expanded']
+            'search_form': form,
+            'total': response.get_num_found(),
+            'items': response.docs,
+            'sort': sort,
+            'page_groups': json.loads(response.get_json())['expanded']
         })
         return context
 
 
+class ItemDetailView(DetailView):
+
+    model = DigitizedWork
+
+    def get_object(self, queryset=None):
+        '''Override get_object to use source_id as lookup criterion'''
+        source_id = self.kwargs.get('id', None)
+        return get_object_or_404(DigitizedWork, source_id=source_id)
+    
