e2527c2f7fa8beb1390428a8c9dffe8200c9f441
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2017-12-13 11:53:24 -0500

    Add DetailView for DigitizedWork + url #7

--- ppa/archive/urls.py
+++ ppa/archive/urls.py
@@ -4,5 +4,6 @@ from ppa.archive import views
 
 
 urlpatterns = [
-    url('^$', views.DigitizedWorkListView.as_view(), name='list'),
+    url('^$', views.ItemListView.as_view(), name='list'),
+    url('^(?P<id>.+)/$', views.ItemDetailView.as_view(), name='detail'),
 ]
