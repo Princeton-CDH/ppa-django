cb3aeb9d376eff4844303ee69c7d7ee77d1590b4
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-12-19 16:36:33 -0500

    Move pytest solr fixtures to shared location

--- ppa/archive/tests/test_commands.py
+++ ppa/archive/tests/test_commands.py
@@ -23,48 +23,9 @@ from ppa.archive.hathi import HathiBibliographicAPI, HathiItemNotFound, \
     HathiBibliographicRecord
 from ppa.archive.models import DigitizedWork
 from ppa.archive.management.commands import hathi_import
-from ppa.archive.solr import SolrSchema, CoreAdmin
 
 
 FIXTURES_PATH = os.path.join(settings.BASE_DIR, 'ppa', 'archive', 'fixtures')
-
-
-@pytest.fixture
-def empty_solr():
-    # pytest solr fixture; updates solr schema
-    with override_settings(SOLR_CONNECTIONS={'default': settings.SOLR_CONNECTIONS['test']}):
-        # reload core before and after to ensure field list is accurate
-        CoreAdmin().reload()
-        solr_schema = SolrSchema()
-        cp_fields = solr_schema.solr.schema.get_schema_copyfields(solr_schema.solr_collection)
-        current_fields = solr_schema.solr_schema_fields()
-
-        for cp_field in cp_fields:
-            solr_schema.solr.schema.delete_copy_field(solr_schema.solr_collection, cp_field)
-        for field in current_fields:
-            # don't delete default/built-in fields!
-            if field == 'id' or field.startswith('_'):
-                continue
-            solr_schema.solr.schema.delete_field(solr_schema.solr_collection, field)
-        CoreAdmin().reload()
-
-        # yield settings so tests run with overridden solr connection
-        yield settings
-
-
-
-@pytest.fixture
-def solr():
-    # pytest solr fixture; updates solr schema
-    with override_settings(SOLR_CONNECTIONS={'default': settings.SOLR_CONNECTIONS['test']}):
-        # reload core before and after to ensure field list is accurate
-        CoreAdmin().reload()
-        SolrSchema().update_solr_schema()
-        CoreAdmin().reload()
-
-        # yield settings so tests run with overridden solr connection
-        yield settings
-
 
 class TestSolrSchemaCommand(TestCase):
 
