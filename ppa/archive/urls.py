0435beb91648bd9489ea4d11c6db20904cb4d953
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-12-20 16:51:32 -0500

    Preliminary CSV export view #13

--- ppa/archive/urls.py
+++ ppa/archive/urls.py
@@ -5,5 +5,6 @@ from ppa.archive import views
 
 urlpatterns = [
     url('^$', views.DigitizedWorkListView.as_view(), name='list'),
+    url('^csv/$', views.DigitizedWorkCSV.as_view(), name='csv'),
     url('^(?P<source_id>.+)/$', views.DigitizedWorkDetailView.as_view(), name='detail'),
 ]
