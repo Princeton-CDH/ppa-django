4eecc113f58b2ea3cbc1db240361a5336084f5fa
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2017-12-20 10:24:26 -0500

    Register DigitizedWorkAutocomplete view #15 Also tidy urls slightly now that we have more

--- ppa/archive/urls.py
+++ ppa/archive/urls.py
@@ -1,9 +1,16 @@
 from django.conf.urls import url
+from django.contrib.admin.views.decorators import staff_member_required
 
 from ppa.archive import views
 
 
 urlpatterns = [
     url('^$', views.DigitizedWorkListView.as_view(), name='list'),
-    url('^(?P<source_id>.+)/$', views.DigitizedWorkDetailView.as_view(), name='detail'),
+    url(
+        '^digitizedworks/autocomplete/$',
+        staff_member_required(views.DigitizedWorkAutocomplete.as_view()),
+        name='digitizedwork-autocomplete',
+    ),
+    url('^digitizedworks/(?P<source_id>.+)/$',
+        views.DigitizedWorkDetailView.as_view(), name='detail'),
 ]
