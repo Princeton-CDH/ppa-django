9e842d6eb923d4afa971ef1107b13f7e72dcf92f
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2017-12-20 10:26:26 -0500

    Add tests for DigitizedWorkAutocomplete #10,#15

--- ppa/archive/tests/test_views.py
+++ ppa/archive/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 import pytest
@@ -85,3 +86,43 @@ class TestArchiveViews(TestCase):
         self.assertContains(response, 'No matching items')
 
 
+class TestDigitizedWorkAutocomplete(TestCase):
+
+    fixtures = ['sample_digitized_works']
+
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_superuser(
+            'foo',
+            'foo@bar.com',
+            'bar'
+        )
+
+    def test_get_queryset_get_result_label(self):
+        '''Tests both what is being returned and that it is being returned
+           so gets at both get_queryset and get_result_label
+        '''
+        autocomplete_url = reverse('archive:digitizedwork-autocomplete')
+
+        # - check access control
+        # anonymous user should get a redirect to login
+        response = self.client.get(autocomplete_url)
+        assert response.status_code == 302
+        # admin user should be allowed in
+        self.client.login(username='foo', password='bar')
+        response = self.client.get(autocomplete_url)
+        assert response.status_code == 200
+        # - test results
+        # empty query string should result in all results (three in this case)
+        data = response.json()
+        assert 'results' in data
+        assert len(data['results']) == 3
+        # pass a query string along -- should get 1 result
+        response = self.client.get(autocomplete_url, {'q': 'wintry'})
+        data = response.json()
+        assert 'results' in data
+        assert len(data['results']) == 1
+        # - examine the text of the result for HTML formatting
+        assert 'text' in data['results'][0]
+        assert data['results'][0]['text'] == \
+            '<strong>Now in wintry delights /</strong><br>chi.13880510'
