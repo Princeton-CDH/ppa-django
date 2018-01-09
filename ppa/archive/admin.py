e727258e738adddec847b40338d2fb677e6bd0f1
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2018-01-09 12:03:31 -0500

    Override save_related to reindex collections #18

--- ppa/archive/admin.py
+++ ppa/archive/admin.py
@@ -14,6 +14,16 @@ class DigitizedWorkAdmin(admin.ModelAdmin):
     # date_hierarchy = 'added'  # is this useful?
     list_filter = ['collections']
 
+    def save_related(self, request, form, formsets, change):
+        '''Ensure reindex is called when admin form is saved'''
+        # m2m relations are handled separately by the form so the standard
+        # save override will not help
+        super(DigitizedWorkAdmin, self).save_related(
+            request, form, formsets, change
+        )
+        digwork = DigitizedWork.objects.get(id=form.instance.pk)
+        digwork.index()
+
 
 admin.site.register(DigitizedWork, DigitizedWorkAdmin)
 admin.site.register(Collection)
