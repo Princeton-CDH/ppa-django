05c5197ee4b855f44f12af43a632924685832d5e
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2018-01-11 10:33:48 -0500

    Revise url routes for clarity and consistency #15 Avoids using digitizedwork in url and keeps tighter in general

--- ppa/archive/urls.py
+++ ppa/archive/urls.py
@@ -4,9 +4,9 @@ from ppa.archive import views
 
 
 urlpatterns = [
-    url('^$', views.DigitizedWorkListView.as_view(), name='list'),
-    url('^digitizedworks/(?P<source_id>.+)/$',
-        views.DigitizedWorkDetailView.as_view(), name='detail'),
     url('^collections/$', views.CollectionListView.as_view(),
         name='list-collections'),
+    url('^(?P<source_id>.+)/$',
+        views.DigitizedWorkDetailView.as_view(), name='detail'),
+    url('^$', views.DigitizedWorkListView.as_view(), name='list'),
 ]
