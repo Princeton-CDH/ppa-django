89b99e7800d01f0af3abc89cc0eae307fe4b29a7
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2018-01-24 12:10:57 -0500

    Test solr mirror and bump to 6.6.2 as result

--- ci/config_solr.sh
+++ ci/config_solr.sh
@@ -1,7 +1,7 @@
 # Config variables local to the script
-version=${SOLR_VERSION:-6.6.0}
+version=${SOLR_VERSION:-6.6.2}
 file="solr-${version}.tgz"
-url="https://archive.apache.org/dist/lucene/solr/${version}/${file}"
+url="http://mirror.reverse.net/pub/apache/lucene/solr/${version}/${file}"
 
 # Check if the file exists for some reason, shouldn't but out of parsimony
 if [ -f $file ];
