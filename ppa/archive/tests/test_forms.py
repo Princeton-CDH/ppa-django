01529fdb41289e4ebe65cdd4fcfb430d87087788
bwhicks <benjamin.w.hicks@gmail.com>
2018-05-09 16:21:20 -0400

    Feature/sort results (#38)
    * Add working draft of seach ordering #29
    
    * Handle custom form init w/o args #29
    
    * Add tests for sort view logic #29
    
    * Update templates form to use spec filter language
    Addresses pull request for #29
    
    * Add disabled radio subclass and tests for #29
    
    * Refactor search form to set solr search fields
    Addresses issues with #29
    
    * Add tests and revise get_solr_sort_field #29
    
    * Expand docstring for get_solr_sort_field #29
    
    * Remove sort notice from template and adjust tests #29
    
    * Use form defaults and default title sort #29

--- ppa/archive/tests/test_forms.py
+++ ppa/archive/tests/test_forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.test import TestCase
 
-from ppa.archive.forms import FacetChoiceField, SearchForm
+from ppa.archive.forms import FacetChoiceField, SearchForm, RadioSelectWithDisabled
 
 
 class TestFacetChoiceField(TestCase):
@@ -41,3 +41,44 @@ class TestSearchForm(TestCase):
             ('foo', 'foo <span>1</span>')
         assert searchform.fields['collections'].choices[1] == \
             ('bar', 'bar <span>2</span>')
+
+    def test_get_solr_fields(self):
+        searchform = SearchForm()
+
+        # try relevance, should return values from dictionaries to set
+        # solr sort field and form/template field
+        sort, solr_sort = searchform.get_solr_sort_field('relevance')
+        assert sort == dict(searchform.SORT_CHOICES)['relevance']
+        assert solr_sort == 'score desc'
+        # try pub_date_asc, should return fields w/o score and set
+        # form template field correctly
+        sort, solr_sort = searchform.get_solr_sort_field('pub_date_asc')
+        assert sort == dict(searchform.SORT_CHOICES)['pub_date_asc']
+        assert solr_sort == 'pub_date asc'
+
+class TestRadioWithDisabled(TestCase):
+
+    def setUp(self):
+
+        class TestForm(forms.Form):
+            '''Build a test form use the widget'''
+            CHOICES = (
+                ('no', {'label': 'no select', 'disabled': True}),
+                ('yes', 'yes can select'),
+            )
+
+            yes_no = forms.ChoiceField(choices=CHOICES,
+                widget=RadioSelectWithDisabled)
+
+        self.form = TestForm()
+
+    def test_create_option(self):
+
+        rendered = self.form.as_p()
+        # no is disabled
+        self.assertInHTML('<input type="radio" name="yes_no" value="no" '
+                          'required id="id_yes_no_0" disabled="disabled" />',
+                          rendered)
+        # yes is not disabled
+        self.assertInHTML('<input type="radio" name="yes_no" value="yes" '
+                          'required id="id_yes_no_1" />', rendered)
