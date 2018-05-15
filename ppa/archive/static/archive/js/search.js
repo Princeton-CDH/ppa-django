772242932ba424183e02e956d7879d0f21e73ac2
Nick Budak <nbudak@princeton.edu>
2018-05-15 12:35:16 -0400

    search form with sort (#63, #64)

--- ppa/archive/static/archive/js/search.js
+++ ppa/archive/static/archive/js/search.js
@@ -1,3 +1,22 @@
 $(function(){
+
+    /* dom */
+    const $searchForm = $('.form')
+    const $sortLinks = $('.sort .item')
+    
+    /* functions */
+    function changeSort(event) {
+        console.log('currently: ', $(event.target).siblings().find('input[checked=""]'))
+        $(event.target).siblings().find('input[checked=""]').removeAttr('checked')
+        $(event.target).find('input').attr('checked', '')
+    }
+    
+    /* bindings */
     $('.ui.checkbox').checkbox()
+    $sortLinks.find('input[checked=""]').parent().addClass('active')
+    $sortLinks.find('input[disabled="disabled"]').parent().addClass('disabled')
+    $sortLinks.click((e) => {
+        changeSort(e)
+        $searchForm.submit()
+    })
 })
