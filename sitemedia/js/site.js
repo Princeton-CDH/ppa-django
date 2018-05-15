92086260acd73db6b27492212141d740720966c2
Nick Budak <nbudak@princeton.edu>
2018-05-15 10:03:12 -0400

    use margin to hide pitbar

--- sitemedia/js/site.js
+++ sitemedia/js/site.js
@@ -12,15 +12,13 @@ $(function(){
             let scroll = checkScroll.scroll || 0
             let scrolled = $(document).scrollTop()
             if (scrolled - scroll > 25 && scrolled > scroll && scrolled > 90) {
-                if ($pitbar.transition('is visible') && !$pitbar.transition('is animating')) {
-                    if (!$mobileNav.sidebar('is visible')) {
-                        $pitbar.transition('slide down')
-                    }
+                if (!$pitbar.hasClass('hidden')) {
+                    $pitbar.addClass('hidden')
                 }
             }
             else if (scrolled < scroll && scroll - scrolled > 5) {
-                if (!$pitbar.transition('is visible') && !$pitbar.transition('is animating')) {
-                    $pitbar.transition('slide down')
+                if ($pitbar.hasClass('hidden')) {
+                    $pitbar.removeClass('hidden')
                 }
             }
             checkScroll.scroll = scrolled // memoize the scroll value
@@ -40,20 +38,3 @@ $(function(){
             }
         })
 })
-
-
-// if ('undefined' !== typeof window.jQuery || 'undefined' !== typeof window.$) {
-//   $(function() {
-//     // ribbon
-//     console.log('init ribbon');
-//     var $ribbon = $('.ribbon');
-//     if ($ribbon) {
-//         var faded = sessionStorage.getItem('fade-test-banner', true);
-//         if (! faded) {
-//             $('.ribbon-box').removeClass('fade');
-//         }
-//         $ribbon.on('click',function(){
-//             $('.ribbon-box').addClass('fade');
-//             sessionStorage.setItem('fade-test-banner', true);
-//         });
-//     }
