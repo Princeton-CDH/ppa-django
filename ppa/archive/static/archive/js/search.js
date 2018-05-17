70cf2745451699ae6caa9a624e607e48a0d128fc
Nick Budak <nbudak@princeton.edu>
2018-05-17 15:22:05 -0400

    basic histogram (#34)

--- ppa/archive/static/archive/js/search.js
+++ ppa/archive/static/archive/js/search.js
@@ -1,9 +1,17 @@
+import DateHistogram from './histogram';
+
 $(function(){
 
     /* dom */
     const $searchForm = $('.form')
     const $sortLinks = $('.sort .item')
     const $clearDatesLink = $('.clear-selection')
+    const $histogram = $('#histogram')
+    // dates
+    let $$minDateInput = $('#id_pub_date_0')
+    let $$maxDateInput = $('#id_pub_date_1')
+    let $$minDate = $('.min-date')
+    let $$maxDate = $('.max-date')
     
     /* functions */
     function changeSort(event) {
@@ -16,6 +24,13 @@ $(function(){
         $('#publication input').val('')
         $searchForm.submit()
     }
+
+    function updateDates() {
+        let minDate = $$minDateInput.val()
+        let maxDate = $$maxDateInput.val()
+        $$minDate.text(minDate != '' ? minDate : $$minDateInput.attr('placeholder'))
+        $$maxDate.text(maxDate != '' ? maxDate : $$maxDateInput.attr('placeholder'))
+    }
     
     /* bindings */
     $('.ui.checkbox').checkbox()
@@ -25,6 +40,12 @@ $(function(){
         changeSort(e)
         $searchForm.submit()
     })
+
     $clearDatesLink.click(clearDates)
+
+    new DateHistogram($histogram)
+
+    // update min and max pub date on visualization
+    updateDates()
 
 })
