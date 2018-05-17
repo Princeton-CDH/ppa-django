ea3fdc5ee75e2e87ad2b66a46c0a73e808b2f243
bwhicks <benjamin.w.hicks@gmail.com>
2018-05-17 11:20:27 -0400

    Feature/title author filter (#73)
    * Re-enable title and author input fields for search form
    
    * Add search form method to check if any keyword terms are present
    
    * Refactor archive solr query logic, add author/title search #67 #68
    
    * Add tests of list view with title/author search
    
    * Test that title, author enable relevance search
    
    * Page join only with text query #67, #68
    
    * Add tests for sort default logic and fix test for collection filter
    
    * Add tests for missing coverage in solr code
    
    * Use py3 super() syntax

--- ppa/archive/tests/test_forms.py
+++ ppa/archive/tests/test_forms.py
@@ -71,6 +71,18 @@ class TestSearchForm(TestCase):
             title='OldProsody', source_id='testppa3', pub_date=1523)
         assert searchform.pub_date_minmax() == expected
 
+    def test_has_keyword_query(self):
+        # no data
+        assert not SearchForm().has_keyword_query({})
+        # non keyword fields
+        assert not SearchForm().has_keyword_query({'pub_date_0': 1800})
+        # any of query, title, author
+        assert SearchForm().has_keyword_query({'query': 'prometheus'})
+        assert SearchForm().has_keyword_query({'title': 'elocution'})
+        assert SearchForm().has_keyword_query({'author': 'bell'})
+        # multiple
+        assert SearchForm().has_keyword_query({'query': 'reading', 'title': 'elocution'})
+
 
 # range widget and field tests copied from derrida, like the objects tested
 
@@ -100,13 +112,11 @@ def test_get_solr_fields():
 
     # try relevance, should return values from dictionaries to set
     # solr sort field and form/template field
-    sort, solr_sort = searchform.get_solr_sort_field('relevance')
-    assert sort == dict(searchform.SORT_CHOICES)['relevance']
+    solr_sort = searchform.get_solr_sort_field('relevance')
     assert solr_sort == 'score desc'
     # try pub_date_asc, should return fields w/o score and set
     # form template field correctly
-    sort, solr_sort = searchform.get_solr_sort_field('pub_date_asc')
-    assert sort == dict(searchform.SORT_CHOICES)['pub_date_asc']
+    solr_sort = searchform.get_solr_sort_field('pub_date_asc')
     assert solr_sort == 'pub_date asc'
 
 
