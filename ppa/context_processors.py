af67baf1a0453fc93d42dd613076bb3b232bbece
rlskoeser <rebecca.s.koeser@princeton.edu>
2018-11-15 11:48:49 -0500

    Add robots.txt adapted from cdhweb #135

--- ppa/context_processors.py
+++ ppa/context_processors.py
@@ -1,11 +1,13 @@
 from django.conf import settings
+from django.contrib.sites.models import Site
 
 
 def template_globals(request):
     '''Template context processor: add global includes (e.g.
-    from django settings or site search form) for us on any page.'''
+    from django settings or site search form) for use on any page.'''
 
     context_extras = {
         'SHOW_TEST_WARNING': getattr(settings, 'SHOW_TEST_WARNING', False),
+        'site': Site.objects.get_current(),
     }
     return context_extras
