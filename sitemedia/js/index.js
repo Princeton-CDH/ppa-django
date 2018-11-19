02dd8321ccc0d936ed33fe9679ffe529473d5b01
Nick Budak <thatbudakguy@gmail.com>
2018-11-19 17:34:54 -0500

    add logotype

--- sitemedia/js/index.js
+++ sitemedia/js/index.js
@@ -6,6 +6,7 @@ $(function(){
     const $mainNav = $('#main-nav')
     const $mobileNav = $('#mobile-nav')
     const $menuButton = $('.toc.item')
+    const $mobileDropdown = $('#mobile-nav .dropdown.item')
 
     /* bindings */
     let pb = new PitBar($mainNav, $mobileNav)
@@ -17,7 +18,11 @@ $(function(){
                 // swap the hamburger icon for a close icon
                 $('.close.icon').toggle()
                 $('.menu.icon').toggle()
-            }
+            },
+            onVisible: () => $('.header.brand .item').addClass('hidden'),
+            onHidden: () => $('.header.brand .item').removeClass('hidden'),
         })
+
+    $mobileDropdown.click(() => $mobileDropdown.toggleClass('active'))
 })
 
