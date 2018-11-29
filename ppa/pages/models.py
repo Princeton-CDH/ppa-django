97c1c77d3fed4e360580f22231d8b47589af3072
rlskoeser <rebecca.s.koeser@princeton.edu>
2018-11-29 17:28:24 -0500

    Fix get_description to ignore empty tags, use *first* text block #159

--- ppa/pages/models.py
+++ ppa/pages/models.py
@@ -99,14 +99,18 @@ class PagePreviewDescriptionMixin(models.Model):
         if there is content, otherwise uses the beginning of the body content.'''
 
         # use description field if set
-        description = self.description.strip()
+        # use striptags to check for empty paragraph)
+        if striptags(self.description):
+            description = self.description
 
         # if not, use beginning of body content
-        if not description:
+        else:
             # Iterate over blocks and use content from the first paragraph content
             for block in self.body:
                 if block.block_type == 'paragraph':
                     description = block
+                    # break so we stop after the first instead of using last
+                    break
 
         # truncate either way
         return truncatechars_html(description, self.max_length)
