ae5b6bea97111201c65033f00dbb659db897f25e
bwhicks <benjamin.w.hicks@gmail.com>
2018-12-04 12:18:58 -0500

    Add and order multiple authors on an editorial (#190)
    * Add person snippet model #150
    
    * Add attribution block to content page #150
    
    * Factor attribution to snippet for editorial #150
    
    * Add tests for authors streamfield #150
    
    * Add unit tests for editorial templates #150
    
    * Add test for author order #150
    
    * Refactor person to pages app and document #150
    
    * Refactor editorial attribution snippet #150
    
    * Fix editorial tests #150

--- ppa/editorial/tests.py
+++ ppa/editorial/tests.py
@@ -1,6 +1,7 @@
 from datetime import date
 
 from django.http import Http404
+from django.test import TestCase
 from wagtail.core.url_routing import RouteResult
 from wagtail.core.models import Site
 from wagtail.tests.utils import WagtailPageTests
@@ -10,6 +11,7 @@ import pytest
 
 from ppa.pages.models import HomePage
 from ppa.editorial.models import EditorialIndexPage, EditorialPage
+from ppa.pages.models import Person
 
 
 class TestEditorialIndexPage(WagtailPageTests):
@@ -88,10 +90,44 @@ class TestEditorialIndexPage(WagtailPageTests):
         editorial_index_url = index_page.relative_url(site)
         response = self.client.get(editorial_index_url)
         assert response.status_code == 200
+        editorial_with_authors = EditorialPage.objects.get(
+            title__icontains='Test Page with Authors'
+        )
         # basic template/content check
+        # NOTE: Should these be refactored into separate template
+        # checking test?
         self.assertContains(response, index_page.title)
         self.assertContains(response, editorial_page.title)
         self.assertContains(response, editorial_page.relative_url(site))
+        # each should be shown once
+        person_a = Person.objects.get(name='Person A')
+        person_b = Person.objects.get(name='Person B')
+        self.assertContains(response, person_a.name, count=1)
+        self.assertContains(response, person_b.name, count=1)
+        # each should only show the 'By' and 'On' twice (for two dates)
+        self.assertContains(response, 'By', count=1)
+        self.assertContains(response, 'On', count=2)
+        # the url for person A should not be present to avoid a
+        # link within a link
+        self.assertNotContains(
+            response,
+            '<a href="%s"' % person_a.url,
+            html=True,
+        )
+        # the date of the post's publication should be present with slash
+        self.assertContains(
+            response,
+            '/ On %s' % editorial_with_authors
+                .first_published_at.strftime('%B %-d, %Y'),
+            count=1
+            )
+        # the date of the post without authors should be present, without slash
+        self.assertContains(
+            response,
+            'On %s' % editorial_page
+                .first_published_at.strftime('%B %-d, %Y'),
+            count=1
+            )
 
         # year only should 404
         response = self.client.get(editorial_index_url + '2018/')
@@ -110,7 +146,6 @@ class TestEditorialIndexPage(WagtailPageTests):
         assert response.status_code == 200
 
         # single-digit month should 404
-        print(editorial_page.relative_url(site).replace('/01/', '/1/'))
         response = self.client.get(editorial_page.relative_url(site).replace('/01/', '/1/'))
         assert response.status_code == 404
 
@@ -128,12 +163,31 @@ class TestEditorialPage(WagtailPageTests):
     def test_can_create(self):
         self.assertCanCreateAt(EditorialIndexPage, EditorialPage)
         parent = EditorialIndexPage.objects.first()
+        # first test without authors, since they're optional
         self.assertCanCreate(parent, EditorialPage, nested_form_data({
             'title': 'Essay',
             'slug': 'essay',
             'date': date.today(),
             'body': streamfield([
                 ('paragraph', rich_text('some analysis'))
+            ]),
+            'authors': streamfield([])
+        }))
+
+        # now check that this can be done with the person snippet,
+        # both with and without url
+        foo = Person.objects.create(name='foo', url='http://bar.com/')
+        bar = Person.objects.create(name='bar')
+        self.assertCanCreate(parent, EditorialPage, nested_form_data({
+            'title': 'Essay2',
+            'slug': 'essay2',
+            'date': date.today(),
+            'body': streamfield([
+                ('paragraph', rich_text('some analysis'))
+            ]),
+            'authors': streamfield([
+                ('author', foo.pk),
+                ('author', bar.pk)
             ])
         }))
 
@@ -155,3 +209,62 @@ class TestEditorialPage(WagtailPageTests):
         url_path = editorial_page.set_url_path(index_page)
         assert date.today().strftime('/%Y/%m/') in url_path
 
+    def test_template_rendering(self):
+        # For EditorialIndexPage's equivalent, see test_routing
+        editorial_page = EditorialPage.objects.get(
+            title__icontains='Test Page with Authors'
+        )
+        site = Site.objects.first()
+        editorial_url = editorial_page.relative_url(site)
+        response = self.client.get(editorial_url)
+        # basic template/content check
+        self.assertContains(response, editorial_page.title)
+        # each author should be shown once
+        person_a = Person.objects.get(name='Person A')
+        person_b = Person.objects.get(name='Person B')
+        self.assertContains(response, person_a.name, count=1)
+        self.assertContains(response, person_b.name, count=1)
+        # each should only show the 'By' and 'On' once
+        self.assertContains(response, 'By', count=1)
+        self.assertContains(response, 'On', count=1)
+        # the date of the post's publication should be present with slash
+        self.assertContains(
+            response,
+            '/ On %s' % editorial_page
+                .first_published_at.strftime('%B %-d, %Y'),
+            count=1
+            )
+        # the url for person A should be present once
+        self.assertContains(response, person_a.url, count=1)
+        # check order of authors in the page context
+        # should be out of alpha order
+        page = response.context['page']
+        # authors are stored as values and not as actual links,
+        # so without the template tag to unpack the stream data,
+        # can check that their value = pk of authors.
+        # order as set in the editorial page should be [B is 2, A is 1]
+        assert page.authors.stream_data[0]['value'] == 2
+        assert page.authors.stream_data[1]['value'] == 1
+        # delete them
+        editorial_page.authors = None
+        editorial_page.save()
+        response = self.client.get(editorial_url)
+        # each author should be shown once
+        self.assertNotContains(response, person_a.name)
+        self.assertNotContains(response, person_b.name)
+        # each should not show the 'By' and 'On' once with date still
+        # the date of the post's publication should be present
+        # without slash
+        self.assertContains(
+            response,
+            'On %s' % editorial_page
+                .first_published_at.strftime('%B %-d, %Y'),
+            count=1
+            )
+
+
+class TestPerson(TestCase):
+
+    def test_str(self):
+        p = Person(name='A person')
+        assert str(p) == 'A person'
