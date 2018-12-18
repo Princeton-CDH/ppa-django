32f871e49f5745695956888132960fe0f1d82261
rlskoeser <rebecca.s.koeser@princeton.edu>
2018-12-18 16:28:49 -0500

    Update code documentation

--- ppa/pages/__init__.py
+++ ppa/pages/__init__.py
@@ -1,5 +1,7 @@
 '''
-Django app for home page and content pages.
+Management and display of home page and other content pages. Includes
+custom page models for collections page, contributor page,
+and a person snippet for contributors and authors.
 '''
 
 default_app_config = 'ppa.pages.apps.PagesConfig'
