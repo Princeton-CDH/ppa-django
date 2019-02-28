33d06099a06fd2b63fdcc530500d3397cd59f0e1
Nick Budak <thatbudakguy@gmail.com>
2019-02-28 16:11:41 -0500

    enumerate all django-webpack-loader default settings

--- ppa/settings.py
+++ ppa/settings.py
@@ -301,8 +301,12 @@ if os.path.exists(f):
 # Django webpack loader
 WEBPACK_LOADER = {
     'DEFAULT': {
-        'BUNDLE_DIR_NAME': 'bundles/',
+        'CACHE': not DEBUG,
+        'BUNDLE_DIR_NAME': 'bundles/', # must end with slash
         'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
+        'POLL_INTERVAL': 0.1,
+        'TIMEOUT': None,
+        'IGNORE': [r'.+\.hot-update.js', r'.+\.map']
     }
 }
 
