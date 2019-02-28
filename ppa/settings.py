668617dcf34cdca795511222a284732382168b86
Nick Budak <thatbudakguy@gmail.com>
2019-02-28 16:04:40 -0500

    add webpack stats file path to settings

--- ppa/settings.py
+++ ppa/settings.py
@@ -302,6 +302,7 @@ if os.path.exists(f):
 WEBPACK_LOADER = {
     'DEFAULT': {
         'BUNDLE_DIR_NAME': 'bundles/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
     }
 }
 
