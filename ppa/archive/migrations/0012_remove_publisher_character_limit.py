ed035a12c4fae4c97d8d6f186ebb6cbbcb0f09be
rlskoeser <rebecca.s.koeser@princeton.edu>
2019-03-04 16:20:18 -0500

    Rename migrations and remove unnecessary merge migration

--- ppa/archive/migrations/0012_remove_publisher_character_limit.py
+++ ppa/archive/migrations/0012_remove_publisher_character_limit.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('archive', '0010_digitizedwork_add_source'),
+        ('archive', '0011_add_protected_fields'),
     ]
 
     operations = [
