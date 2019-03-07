280ca38996c70337b6c2130cce6af19bf19774c4
rlskoeser <rebecca.s.koeser@princeton.edu>
2019-03-07 16:03:54 -0500

    Add tests for AjaxTemplateMixin

--- ppa/common/views.py
+++ ppa/common/views.py
@@ -1,5 +1,5 @@
 from django.utils.cache import patch_vary_headers
-from django.views.generic.base import View
+from django.views.generic.base import View, TemplateResponseMixin
 
 
 class VaryOnHeadersMixin(View):
@@ -19,15 +19,20 @@ class VaryOnHeadersMixin(View):
         return response
 
 
-class AjaxTemplateMixin(VaryOnHeadersMixin):
+class AjaxTemplateMixin(TemplateResponseMixin, VaryOnHeadersMixin):
     '''View mixin to return a different version of a view's template when
     responding to an ajax request.
     '''
 
+    #: name of the template to use for ajax request
     ajax_template_name = None
+    #: vary on X-Request-With to avoid browsers caching and displaying
+    #: ajax response for the non-ajax response
     vary_headers = ['X-Requested-With']
 
     def get_template_names(self):
+        '''Return :attr:`ajax_template_name` if this is an ajax request;
+        otherwise return default template name.'''
         if self.request.is_ajax():
             return self.ajax_template_name
         return super().get_template_names()
