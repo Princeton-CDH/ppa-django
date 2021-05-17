bdaa22c634b2afd7e21c15b8e0cc8f195d4f0486
Nick Budak <nbudak@princeton.edu>
2021-05-17 16:27:05 -0400

    Switch CI to GitHub Actions (#338)

--- ci/testsettings.py
+++ ci/testsettings.py
@@ -4,6 +4,7 @@
 # If this file is changed in development, the development server will
 # have to be manually restarted because changes will not be noticed
 # immediately.
+import os
 
 DEBUG = False
 
@@ -11,17 +12,21 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test',
+        'NAME': 'ppa',
+        'PASSWORD': 'ppa',
         'USER': 'root',
-        'HOST': 'localhost',
-        'PORT': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            # In each case, we want strict mode on to catch truncation issues
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
         'TEST': {
             'CHARSET': 'utf8',
             'COLLATION': 'utf8_general_ci',
         },
     },
 }
-
 
 # required by mezzanine for unit tests
 ALLOWED_HOSTS = ['*']
@@ -30,12 +35,18 @@ ALLOWED_HOSTS = ['*']
 SOLR_CONNECTIONS = {
     'default': {
         'URL': 'http://localhost:8983/solr/',
-        'COLLECTION': 'ppa-pa11y',
+        'COLLECTION': 'ppa',
         'CONFIGSET': 'ppa',
         'TEST': {
-            'COLLECTION': 'test-ppa',
             'COMMITWITHIN': 100
         }
+    }
+}
+
+# use an empty webpack-stats.json so we can ignore missing static files in tests
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'STATS_FILE': os.path.join(BASE_DIR, 'ci', 'webpack-stats.json')
     }
 }
 
