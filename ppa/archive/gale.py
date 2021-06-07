f27f5bc72bebe6682fabc1387b97086d1fe1d2e0
rlskoeser <rebecca.s.koeser@princeton.edu>
2021-06-07 17:48:40 -0400

    Preliminary Gale API client

---
+++ ppa/archive/gale.py
@@ -0,0 +1,98 @@
+import logging
+import time
+
+import requests
+
+from django.conf import settings
+
+
+from ppa import __version__ as ppa_version
+
+
+logger = logging.getLogger(__name__)
+
+
+class GaleItemForbidden(Exception):
+    '''Permission denied to access item in data API'''
+
+
+class GaleAPI:
+
+    #: base URL for all API requests
+    api_root = "https://api.gale.com/api"
+
+    def __init__(self):
+        # NOTE: copied from hathi.py base api class; generalize?
+
+        # create a request session, for request pooling
+        self.session = requests.Session()
+        # set a user-agent header, but  preserve requests version information
+        headers = {
+            "User-Agent": "ppa-django/%s (%s)"
+            % (ppa_version, self.session.headers["User-Agent"])
+        }
+        # include technical contact as From header, if set
+        tech_contact = getattr(settings, "TECHNICAL_CONTACT", None)
+        if tech_contact:
+            headers["From"] = tech_contact
+        self.session.headers.update(headers)
+        # error handling?
+        # username is needed to request an API key
+        self.username = settings.GALE_API_USERNAME
+
+    def _make_request(self, url, params=None, requires_api_key=True):
+        """Make a GET request with the configured session. Takes a url
+        relative to :attr:`api_root` and optional dictionary of parameters."""
+        # NOTE: also copied from hathi.py
+
+        # Returns the response for status 200 OK; raises
+        # :class:`HathiItemNotFound` for 404 and :class:`HathiItemForbidden`
+        # for 403.
+        # '''
+        url = "%s/%s" % (self.api_root, url)
+        rqst_opts = {}
+        if params:
+            rqst_opts["params"] = params
+
+        # add api key to parameters if neded for this request
+        if requires_api_key:
+            if "params" not in rqst_opts:
+                rqst_opts["params"] = {}
+            rqst_opts["params"]["api_key"] = self.api_key
+            print(rqst_opts)
+
+        start = time.time()
+        resp = self.session.get(url, **rqst_opts)
+        logger.debug("get %s %s: %f sec", url, resp.status_code, time.time() - start)
+        if resp.status_code == requests.codes.ok:
+            return resp
+        # if resp.status_code == requests.codes.not_found:
+        #     raise HathiItemNotFound
+        if resp.status_code == requests.codes.forbidden:
+            # forbidden results return a message
+            # NOTE that item requests for invalid ids return 403
+            # TODO: add logic to check if api key has expired and get a new one?
+            # (if we expect to ever have anything that will take longer than 30m)
+            raise GaleItemForbidden(resp.json()['message'])
+
+    def get_api_key(self):
+        # GALE API requires use of an API key, which lasts for 30 minutes
+        # request a new one when needed using configured username
+        response = self._make_request(
+            "tools/generate_key", {"user": self.username}, requires_api_key=False
+        )
+        return response.json()["apiKey"]
+
+    _api_key = None
+
+    @property
+    def api_key(self):
+        """Property for curren tapi key. Requests a new one when needed."""
+        if self._api_key is None:
+            self._api_key = self.get_api_key()
+        return self._api_key
+
+    def get_item(self, id):
+        # numeric ids need CW added; unclear if that is part of the id or not
+        response = self._make_request(f"v1/item/GALE%7C{id}")
+        return response.json()
