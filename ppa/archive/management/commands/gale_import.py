29a98f4704fc2a63bb7b85808e846468176b6bae
rlskoeser <rebecca.s.koeser@princeton.edu>
2021-06-08 14:51:37 -0400

    Refine import script argument handling; create log entry on import

--- ppa/archive/management/commands/gale_import.py
+++ ppa/archive/management/commands/gale_import.py
@@ -7,6 +7,10 @@ materials into the local database for management.
 import csv
 from collections import Counter
 
+from django.conf import settings
+from django.contrib.admin.models import LogEntry, ADDITION
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand, CommandError
 from django.template.defaultfilters import pluralize, truncatechars
 from parasolr.django.signals import IndexableSignalHandler
@@ -35,11 +39,9 @@ class Command(BaseCommand):
             help="Optional list of specific items to import by Gale id.",
         )
         parser.add_argument(
-            "--csv", type=str, help="CSV file with items to import be imported."
+            "-c", "--csv", type=str, help="CSV file with items to import be imported."
         )
-
-        # TODO: require one and only one of ids or csv argument
-
+        # TODO: do we need to support update?
         # parser.add_argument(
         #     '-u', '--update', action='store_true',
         #     help='Update local content even if source record has not changed.')
@@ -48,12 +50,18 @@ class Command(BaseCommand):
         #     help='Display a progress bar to track the status of the import.')
 
     def handle(self, *args, **kwargs):
+
+        if not (kwargs["ids"] or kwargs["csv"]):
+            raise CommandError('A list of IDs or CSV file for is required for import')
+
         # disconnect signal handler for on-demand indexing, for efficiency
         # (index in bulk after an update, not one at a time)
         IndexableSignalHandler.disconnect()
 
         self.gale_api = GaleAPI()
         self.stats = Counter()
+        self.script_user = User.objects.get(username=settings.SCRIPT_USERNAME)
+        self.digwork_contentype = ContentType.objects.get_for_model(DigitizedWork)
 
         # if ids are specified on the command line, create a list
         # of dictionaries so import will look similar to csv
@@ -62,10 +70,8 @@ class Command(BaseCommand):
         # when csv is specified, load rows into a list of dics
         elif kwargs["csv"]:
             to_import = self.load_csv(kwargs["csv"])
-        else:
-            raise CommandError("Must specify items to import by id or CSV file")
 
-        # needed for progessbar
+        # total is needed for progessbar (if we add it)
         self.stats["total"] = len(to_import)
 
         for item in to_import:
@@ -142,19 +148,17 @@ class Command(BaseCommand):
             ),
         )
 
-        # try:
-        #     digwork = DigitizedWork.add_from_hathi(
-        #         htid, self.bib_api, update=self.options['update'],
-        #         log_msg_src='via hathi_import script')
-        # except HathiItemNotFound:
-        #     self.stdout.write("Error: Bibliographic data not found for '%s'" % htid)
-        #     self.stats['error'] += 1
-        #     return
+        # create log entry to document import
+        LogEntry.objects.log_action(
+            user_id=self.script_user.pk,
+            content_type_id=self.digwork_contentype.pk,
+            object_id=digwork.pk,
+            object_repr=str(digwork),
+            change_message='Created from Gale API',
+            action_flag=ADDITION)
 
-        # TODO: create log entry to document import
-
-        # NOTE: item record includes page metadata; we should index
-        # at the same time as import
+        # NOTE: item record includes page metadata; would be more
+        # efficient to index at the same time as import, if possible
 
         self.stats["imported"] += 1
         self.stats["pages"] += digwork.page_count
