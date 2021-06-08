51b13ded81a6867895dfe4d1fe1f51dfd14fdbd8
rlskoeser <rebecca.s.koeser@princeton.edu>
2021-06-08 17:16:50 -0400

    Add collection assignment based on spreadsheet; skip if already in db

--- ppa/archive/management/commands/gale_import.py
+++ ppa/archive/management/commands/gale_import.py
@@ -12,11 +12,12 @@ from django.contrib.admin.models import LogEntry, ADDITION
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand, CommandError
+from django.db.utils import IntegrityError
 from django.template.defaultfilters import pluralize, truncatechars
 from parasolr.django.signals import IndexableSignalHandler
 
 from ppa.archive.gale import GaleAPI, GaleAPIError
-from ppa.archive.models import DigitizedWork
+from ppa.archive.models import Collection, DigitizedWork
 
 
 class Command(BaseCommand):
@@ -31,6 +32,18 @@ class Command(BaseCommand):
     #: normal verbosity level
     v_normal = 1
     verbosity = v_normal
+
+    # input spreadsheets use the following codes as field names
+    # to indicate collection membership
+    collection_codes = {
+        "OB": "Original Bibliography",
+        "LIT": "Literary",
+        "MUS": "Music",
+        "TYP": "Typographically Unique",
+        "LING": "Linguistic",
+        "DIC": "Dictionaries",
+        "WL": "Word Lists",
+    }
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -52,7 +65,7 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
 
         if not (kwargs["ids"] or kwargs["csv"]):
-            raise CommandError('A list of IDs or CSV file for is required for import')
+            raise CommandError("A list of IDs or CSV file for is required for import")
 
         # disconnect signal handler for on-demand indexing, for efficiency
         # (index in bulk after an update, not one at a time)
@@ -70,6 +83,8 @@ class Command(BaseCommand):
         # when csv is specified, load rows into a list of dics
         elif kwargs["csv"]:
             to_import = self.load_csv(kwargs["csv"])
+            # load collections when importing from CSV
+            self.load_collections()
 
         # total is needed for progessbar (if we add it)
         self.stats["total"] = len(to_import)
@@ -77,23 +92,34 @@ class Command(BaseCommand):
         for item in to_import:
             if self.verbosity >= self.v_normal:
                 # include title in output if present, but truncate since many are long
-                self.stdout.write(' '.join([item["ID"], truncatechars(item.get("Title", ''), 55)]))
+                self.stdout.write(
+                    " ".join([item["ID"], truncatechars(item.get("Title", ""), 55)])
+                )
             self.import_digitizedwork(item["ID"], **item)
 
         summary = (
             "\nProcessed {:,d} item{} for import."
-            + "\nImported {:,d}; {:,d} error{}; imported {:,d} page{}."
+            + "\nImported {:,d}; skipped {:,d}; {:,d} error{}; imported {:,d} page{}."
         )
         summary = summary.format(
             self.stats["total"],
             pluralize(self.stats["total"]),
             self.stats["imported"],
+            self.stats["skipped"],
             self.stats["error"],
             pluralize(self.stats["error"]),
             self.stats["pages"],
             pluralize(self.stats["pages"]),
         )
         self.stdout.write(summary)
+
+    def load_collections(self):
+        # load collections from the database and create
+        # a lookup based on the codes used in the spreadsheet
+        self.collections = {}
+        collections = {c.name: c for c in Collection.objects.all()}
+        for code, name in self.collection_codes.items():
+            self.collections[code] = collections[name]
 
     def load_csv(self, path):
         """Load a CSV file with items to be imported."""
@@ -111,6 +137,14 @@ class Command(BaseCommand):
     def import_digitizedwork(self, gale_id, **kwargs):
         """Import a single work into the database.
         Retrieves bibliographic data from Gale API."""
+
+        # if an item with this source id exists, skip
+        # (check local db first because API call is slow for large items)
+        # NOTE: revisit if we decide to support update logic
+        if DigitizedWork.objects.filter(source_id=gale_id).exists():
+            self.stderr.write("%s is already in the database; skipping" % gale_id)
+            self.stats["skipped"] += 1
+            return
 
         try:
             item_record = self.gale_api.get_item(gale_id)
@@ -147,6 +181,13 @@ class Command(BaseCommand):
                 [n for n in (kwargs.get("NOTES"), doc_metadata["citation"]) if n]
             ),
         )
+        # set collection membership based on spreadsheet columns
+        digwork_collections = [
+            collection
+            for code, collection in self.collections.items()
+            if kwargs.get(code)
+        ]
+        digwork.collections.set(digwork_collections)
 
         # create log entry to document import
         LogEntry.objects.log_action(
@@ -154,8 +195,9 @@ class Command(BaseCommand):
             content_type_id=self.digwork_contentype.pk,
             object_id=digwork.pk,
             object_repr=str(digwork),
-            change_message='Created from Gale API',
-            action_flag=ADDITION)
+            change_message="Created from Gale API",
+            action_flag=ADDITION,
+        )
 
         # NOTE: item record includes page metadata; would be more
         # efficient to index at the same time as import, if possible
