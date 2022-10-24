8fe1bbdae8f8464caba773fe49ddc92211026128
Ryan Heuser <ryan.heuser@gmail.com>
2022-10-24 17:22:57 -0400

    psql-based tests need their own settings.py

---
+++ ci/testsettings-psql.py
@@ -0,0 +1,50 @@
+# This file is exec'd from settings.py, so it has access to and can
+# modify all the variables in settings.py.
+
+# If this file is changed in development, the development server will
+# have to be manually restarted because changes will not be noticed
+# immediately.
+import os
+
+DEBUG = False
+
+# include database settings to use Mariadb ver on production (5.5)
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "ppa",
+        "USER": "ppa",
+        "PASSWORD": "ppa",
+        "USER": "root",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "OPTIONS": {
+            # In each case, we want strict mode on to catch truncation issues
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+        "TEST": {
+            "CHARSET": "utf8",
+            "COLLATION": "utf8_general_ci",
+        },
+    },
+}
+
+# required by mezzanine for unit tests
+ALLOWED_HOSTS = ["*"]
+
+# required for integration tests that query Solr
+SOLR_CONNECTIONS = {
+    "default": {
+        "URL": "http://localhost:8983/solr/",
+        "COLLECTION": "ppa",
+        "CONFIGSET": "ppa",
+        "TEST": {"COMMITWITHIN": 100},
+    }
+}
+
+# use an empty webpack-stats.json so we can ignore missing static files in tests
+WEBPACK_LOADER = {
+    "DEFAULT": {"STATS_FILE": os.path.join(BASE_DIR, "ci", "webpack-stats.json")}
+}
+
+# secret key added as a travis build step
