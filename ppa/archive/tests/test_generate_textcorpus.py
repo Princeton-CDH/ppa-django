e5feb3efac8aa5700e61a0fb9bad5c9a61b6d4bf
Ryan Heuser <ryan.heuser@princeton.edu>
2023-12-05 12:01:02 -0500

    fixes, but can't get mock to match

--- ppa/archive/tests/test_generate_textcorpus.py
+++ ppa/archive/tests/test_generate_textcorpus.py
@@ -1,9 +1,9 @@
 from unittest.mock import patch
-import json
 import pytest
 from django.core.management import call_command
 from django.core.management.base import CommandError
 import os
+import jsonlines,json
 
 # mock results for acet query used to get document IDs and page counts
 mock_solr_facets = {"group_id_s": {"doc_1": 2, "doc_2": 1}}
@@ -12,20 +12,22 @@ mock_solr_facets = {"group_id_s": {"doc_1": 2, "doc_2": 1}}
 mock_solr_docs = [
     # The first record has item_type='work' and contains metadata for the
     # document
-    {"item_type": "work", "pub_year": 1863, "group_id_s":"doc_1"},
-    {"item_type": "work", "pub_year": "unknown","group_id_s":"doc_2"},
+    {"item_type": "work", "pub_year": 1863, "group_id_s":"doc_1", 'id':'yyy'},
+    {"item_type": "work", "pub_year": "unknown","group_id_s":"doc_2", 'id':'xxx'},
     # If multiple metadata rows are found, the first one (above) is used
     # Subsequent records have item_type='page', page-order specified by
     # 'order', with content in 'content'
     {
+        'id':'yyy.001',
         "item_type": "page",
         "order": 1,
         "content": "Four score and seven years ago our fathers brought forth"
         " on this continent, a new nation, ",
         "group_id_s":"doc_1",
-        "label":'i'
+        "label":'i',
     },
     {
+        'id':'xxx.001',
         "item_type": "page",
         "order": 2,
         "content": "conceived in Liberty, and dedicated to the proposition"
@@ -37,6 +39,7 @@ mock_solr_docs = [
 
 
     {
+        'id':'xxx.001',
         "item_type": "page",
         "order": 3,
         "content": "!!!!!",
@@ -55,35 +58,26 @@ def patched_solr_queryset(mock_solr_queryset):
         "ppa.archive.management.commands.generate_textcorpus.SolrQuerySet", new=mock_qs
     ) as mock_queryset_cls:
         mock_qs = mock_queryset_cls.return_value
-        mock_qs.get_results.return_value = mock_solr_docs
-        mock_qs.get_facets.return_value.facet_fields = mock_solr_facets
-
+        mock_qs.only.return_value.count.return_value = len(mock_solr_docs)
+        mock_qs.only.return_value.get_results.return_value = mock_solr_docs
         yield mock_qs
 
 
 def test_save(tmpdir, patched_solr_queryset):
-    call_command("generate_textcorpus", "--path", tmpdir.dirpath())
-    metadata_file = tmpdir.dirpath("metadata.csv")
-    assert metadata_file.check()
+    call_command("generate_textcorpus", "--path", tmpdir.dirpath(),'--doc-limit',10)
+    print(os.listdir(tmpdir.dirpath()))
     
-    with open(metadata_file) as f: 
-        meta=json.load(f)
-    assert len(meta) == 2
+    metadata_file = tmpdir.dirpath("metadata.json")
+    pages_file = tmpdir.dirpath("pages.jsonl")
+    assert metadata_file.check()
+    with open(metadata_file) as f: meta=json.load(f)
+    assert len(meta) == 10
 
-    tdir=tmpdir.dirpath('texts')
-    fns=os.listdir(tdir)
-    assert len(fns) == 2
-
-    fn1=os.path.join(tdir,fns[0])
-    fn2=os.path.join(tdir,fns[1])
-    with open(fn1) as f: ld1=json.load(f)
-    with open(fn2) as f: ld2=json.load(f)
-
-    assert len(ld1)==2
-    assert len(ld2)==1
-
-    assert all(all(bool(v) for k,v in d.items()) for d in ld1)
-    assert all(all(bool(v) for k,v in d.items()) for d in ld2)
+    def numlines(fngz):
+        with gzip.open(fngz,'rt',encoding='utf-8') as f:
+            return sum(1 for ln in f)
+    
+    assert numlines(pages_file) > 10
         
 
 
