8fa7e69388b9dd50e26b098ffab7a8fab3e3cce1
Ryan Heuser <ryan.heuser@princeton.edu>
2023-12-09 15:30:35 -0500

    batching does not improve save speed

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
+# sys.path.insert(0,'/Users/ryanheuser/github/parasolr')
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ppa.settings")
