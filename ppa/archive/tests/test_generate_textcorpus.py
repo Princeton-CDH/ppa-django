0b44a15616e09303d243b4c2f97c96c003a82a0b
Ryan Heuser <ryan.heuser@princeton.edu>
2023-12-10 08:21:30 -0500

    improvements precommit 2

--- ppa/archive/tests/test_generate_textcorpus.py
+++ ppa/archive/tests/test_generate_textcorpus.py
@@ -4,44 +4,42 @@ from django.core.management import call_command
 from django.core.management.base import CommandError
 import os
 import json
+import gzip
 
 # mock result for solr document data
 mock_solr_docs = [
     # The first record has item_type='work' and contains metadata for the
     # document
-    {"item_type": "work", "pub_year": 1863, "group_id_s":"doc_1", 'id':'yyy'},
-    {"item_type": "work", "pub_year": "unknown","group_id_s":"doc_2", 'id':'xxx'},
+    {"item_type": "work", "pub_year": 1863, "group_id_s": "doc_1", "id": "yyy"},
+    {"item_type": "work", "pub_year": "unknown", "group_id_s": "doc_2", "id": "xxx"},
     # If multiple metadata rows are found, the first one (above) is used
     # Subsequent records have item_type='page', page-order specified by
     # 'order', with content in 'content'
     {
-        'id':'yyy.001',
+        "id": "yyy.001",
         "item_type": "page",
         "order": 1,
         "content": "Four score and seven years ago our fathers brought forth"
         " on this continent, a new nation, ",
-        "group_id_s":"doc_1",
-        "label":'i',
+        "group_id_s": "doc_1",
+        "label": "i",
     },
     {
-        'id':'xxx.001',
+        "id": "xxx.001",
         "item_type": "page",
         "order": 2,
         "content": "conceived in Liberty, and dedicated to the proposition"
         " that all men are created equal.",
-        "group_id_s":"doc_1",
-        "label":'ii'
+        "group_id_s": "doc_1",
+        "label": "ii",
     },
-
-
-
     {
-        'id':'xxx.001',
+        "id": "xxx.001",
         "item_type": "page",
         "order": 3,
         "content": "!!!!!",
-        "group_id_s":"doc_2",
-        "label":"2"
+        "group_id_s": "doc_2",
+        "label": "2",
     },
 ]
 
@@ -61,34 +59,29 @@ def patched_solr_queryset(mock_solr_queryset):
 
 
 def test_save(tmpdir, patched_solr_queryset):
-    call_command("generate_textcorpus", "--path", tmpdir.dirpath(),'--doc-limit',10)
+    call_command("generate_textcorpus", "--path", tmpdir.dirpath(), "--doc-limit", 10)
     print(os.listdir(tmpdir.dirpath()))
-    
+
     metadata_file = tmpdir.dirpath("metadata.json")
     pages_file = tmpdir.dirpath("pages.jsonl")
     assert metadata_file.check()
-    with open(metadata_file) as f: meta=json.load(f)
+    with open(metadata_file) as f:
+        meta = json.load(f)
     assert len(meta) == 2
 
     def numlines(fngz):
-        with gzip.open(fngz,'rt',encoding='utf-8') as f:
+        with gzip.open(fngz, "rt", encoding="utf-8") as f:
             return sum(1 for ln in f)
-    
+
     assert numlines(pages_file) == 3
-        
-
-
-
 
 
 def test_invalid_preprocess_flags(tmpdir, patched_solr_queryset):
     # Flags that are not supported
     with pytest.raises(CommandError):
         call_command(
-            "generate_textcorpus", "--path", tmpdir.dirpath(), "--doc-limit","one"
+            "generate_textcorpus", "--path", tmpdir.dirpath(), "--doc-limit", "one"
         )
 
     with pytest.raises(CommandError):
-        call_command(
-            "generate_textcorpus", "--woops","huh"
-        )
+        call_command("generate_textcorpus", "--woops", "huh")
