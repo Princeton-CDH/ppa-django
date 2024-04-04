ed0f4d8b69cbf416ab84a1a6a3c30cf4b8065eee
rlskoeser <rebecca.s.koeser@princeton.edu>
2024-04-04 15:31:23 -0400

    Actually commit import of debug_toolbar to check for import error
    - mark so ruff does not clean up unused import

--- ppa/settings/components/debug.py
+++ ppa/settings/components/debug.py
@@ -5,6 +5,8 @@ INTERNAL_IPS = ["127.0.0.1", "localhost"]
 
 # if django-debug-toolbar is installed, enable it
 try:
+    import debug_toolbar  # noqa: F401  (do not clean up unused import)
+
     INSTALLED_APPS.append("debug_toolbar")
     MIDDLEWARE += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
 except ImportError:
