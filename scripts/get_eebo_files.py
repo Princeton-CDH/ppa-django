9ceb69cb12fd538e592a5358cf3f679913ab6541
rlskoeser <rebecca.s.koeser@princeton.edu>
2024-05-09 10:30:56 -0400

    Refactor common phase1/2 logic; move xml extraction into a function

--- scripts/get_eebo_files.py
+++ scripts/get_eebo_files.py
@@ -18,13 +18,110 @@ Setup:
 - Extract to the same directory with EEBO-TCP content; expected file name
   is `IDmap.eebo_2015.xml`
 
+Example usage::
+
+    ./scripts/get_eebo_files.py eebo_works.csv data/eebo_tcp/ /tmp/eebo
+
 """
 
 import argparse
 import csv
 from collections import defaultdict
+from os.path import basename
 from pathlib import Path
 from zipfile import ZipFile
+
+
+def group_ids(idlist: list) -> dict[str, list]:
+    # given a list of of EEBO-TCP ids, return a dictionary of lists
+    # keyed on first two letters of id.
+    # zipfile names are based on first two letters of id, e.g. A0.zip
+    grouped_ids = defaultdict(list)
+    for volume_id in idlist:
+        prefix = volume_id[:2]
+        grouped_ids[prefix].append(volume_id)
+
+    return grouped_ids
+
+
+def extract_from_zipfile(
+    zipfilename: str, extract_list: list, output_path: Path
+) -> int:
+    """Takes a path to zipfile, a list of files in that zip to extract,
+    and an output path where files should be extracted. Writes
+    files to a single directory without nesting.
+    Returns a count of the number of files extracted.
+    """
+    count = 0
+    with ZipFile(zipfilename) as eebozip:
+        for extract_path in extract_list:
+            # zipfile.extract would be convenient, but preserves
+            # nested directory structure; we want to export to a single directory
+            # paths inside the zip file are nested, but we want to extract
+            # to a single directory; use full path to read, basename to write
+            output_name = basename(extract_path)
+            with open(output_path / output_name, "wb") as outfile:
+                outfile.write(eebozip.read(extract_path))
+            count += 1
+    return count
+
+
+def get_xml_files(eebo_path: Path, base_ids: list, output_dir: Path):
+    print(f"Exporting XML for {len(base_ids)} unique volumes to {output_dir}")
+
+    # make sure we can use the specified output path
+    if not output_path.exists():
+        output_path.mkdir()
+    elif not output_path.is_dir():
+        raise SystemExit(f"Output path {output_path} exists but is not a directory")
+
+    # determine which ids are phase 1 / 2 so we know where to look for them
+    phase1_id_file = eebo_path / "IDnos_in_phase1.txt"
+    phase2_id_file = eebo_path / "IDnos_in_phase2.txt"
+
+    # read in phase 1 ids and identify volume ids to import from phase 1
+    with phase1_id_file.open() as idfile:
+        phase1_ids = set(idfile.read().splitlines())
+        phase1_import = base_ids.intersection(phase1_ids)
+
+    # same thing for phase 1
+    with phase2_id_file.open() as idfile:
+        phase2_ids = set(idfile.read().splitlines())
+        phase2_import = base_ids.intersection(phase2_ids)
+
+    phase1_xml = eebo_path / "P4_XML_TCP"
+    phase2_xml = eebo_path / "P4_XML_TCP_Ph2"
+    # group ids by file prefix for easy matching with zip file names
+    phase1_file_ids = group_ids(list(phase1_import))
+    phase2_file_ids = group_ids(list(phase2_import))
+
+    phase1_count = 0
+    for file_id, volume_ids in phase1_file_ids.items():
+        # zipfile name is based on first two letters of id, e.g. A0.zip
+        eebo_zipfile = phase1_xml / f"{file_id}.zip"
+        extract_files = [f"{file_id}/{volume_id}.P4.xml" for volume_id in volume_ids]
+        phase1_count += extract_from_zipfile(eebo_zipfile, extract_files, output_path)
+    print(f"Exported {phase1_count} XML files from EEBO-TCP phase 1")
+
+    phase2_count = 0
+    for file_id, volume_ids in phase2_file_ids.items():
+        # zipfile name is based on first two letters of id, e.g. A0.zip
+        eebo_zipfile = phase2_xml / f"{file_id}.zip"
+        extract_files = [f"{file_id}/{volume_id}.P4.xml" for volume_id in volume_ids]
+        phase2_count += extract_from_zipfile(eebo_zipfile, extract_files, output_path)
+
+    print(f"Exported {phase2_count} XML files from EEBO-TCP phase 2")
+
+    total_exported = phase1_count + phase2_count
+    if total_exported == len(base_ids):
+        print(f"Successfully exported all {total_exported} volumes")
+    else:
+        # unlikely we get here, if there's an error the script will probably crash,
+        # but report anyway just in case
+        print(f"Only exported {total_exported} volumes, something went wrong...")
+
+
+# def get_marcl_files(args.eebo_path, base_ids, args.output_dir):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -34,10 +131,10 @@ if __name__ == "__main__":
         "input", help="CSV file with EEBO IDs to select as column 'Volume ID'"
     )
     parser.add_argument(
-        "eebo_path", help="Path to location where EEBO-TCP content has been extracted"
+        "eebo_dir", help="Directory where EEBO-TCP content has been extracted"
     )
     parser.add_argument(
-        "output", help="directory where the selected files should be saved"
+        "output_dir", help="Directory where the selected files should be saved"
     )
     args = parser.parse_args()
 
@@ -53,80 +150,8 @@ if __name__ == "__main__":
     # There are duplicates due to multiple excerpts from the same volume.
     base_ids = set([vol.split(".")[0] for vol in volume_ids])
 
-    print(f"Exporting XML for {len(base_ids)} unique volumes to {args.output}")
+    # convert directory args to path objects since needed for both steps
+    eebo_path = Path(args.eebo_dir)
+    output_path = Path(args.output_dir)
 
-    eebo_dir = Path(args.eebo_path)
-
-    # determine which ids are phase 1 / 2 so we know where to look for them
-    phase1_id_file = eebo_dir / "IDnos_in_phase1.txt"
-    phase2_id_file = eebo_dir / "IDnos_in_phase2.txt"
-
-    # read in phase 1 ids and identify volume ids to import from phase 1
-    with phase1_id_file.open() as idfile:
-        phase1_ids = set(idfile.read().splitlines())
-        phase1_import = base_ids.intersection(phase1_ids)
-
-    # same thing for phase 1
-    with phase2_id_file.open() as idfile:
-        phase2_ids = set(idfile.read().splitlines())
-        phase2_import = base_ids.intersection(phase2_ids)
-
-    output_path = Path(args.output)
-    if not output_path.exists():
-        output_path.mkdir()
-    elif not output_path.is_dir():
-        raise SystemExit(f"Output path {output_path} exists but is not a directory")
-
-    phase1_xml = eebo_dir / "P4_XML_TCP"
-    # group ids by file prefix
-    phase1_file_ids = defaultdict(list)
-    for volume_id in list(phase1_import):
-        # zipfile name is based on first two letters of id, e.g. A0.zip
-        prefix = volume_id[:2]
-        phase1_file_ids[prefix].append(volume_id)
-
-    phase1_count = 0
-    for file_id, volume_ids in phase1_file_ids.items():
-        # zipfile name is based on first two letters of id, e.g. A0.zip
-        eebo_zipfile = phase1_xml / f"{file_id}.zip"
-
-        with ZipFile(eebo_zipfile) as eebozip:
-            for volume_id in volume_ids:
-                xml_filename = f"{volume_id}.P4.xml"
-                zip_xml_filename = f"{file_id}/{xml_filename}"
-                # zipfile.extract would be convenient, but preserves
-                # nested directory structure; export to a single directory
-                with open(output_path / xml_filename, "wb") as outfile:
-                    outfile.write(eebozip.read(zip_xml_filename))
-                phase1_count += 1
-    print(f"Exported {phase1_count} XML files from EEBO-TCP phase 1")
-
-    # then do the same for phase 2
-    phase2_xml = eebo_dir / "P4_XML_TCP_Ph2"
-    # group ids by file prefix
-    phase2_file_ids = defaultdict(list)
-    for volume_id in list(phase2_import):
-        # zipfile name is based on first two letters of id, e.g. A0.zip
-        prefix = volume_id[:2]
-        phase2_file_ids[prefix].append(volume_id)
-
-    phase2_count = 0
-    for file_id, volume_ids in phase2_file_ids.items():
-        # zipfile name is based on first two letters of id, e.g. A0.zip
-        eebo_zipfile = phase2_xml / f"{file_id}.zip"
-
-        with ZipFile(eebo_zipfile) as eebozip:
-            for volume_id in volume_ids:
-                xml_filename = f"{volume_id}.P4.xml"
-                zip_xml_filename = f"{file_id}/{xml_filename}"
-                with open(output_path / xml_filename, "wb") as outfile:
-                    outfile.write(eebozip.read(zip_xml_filename))
-                phase2_count += 1
-
-    print(f"Exported {phase2_count} XML files from EEBO-TCP phase 2")
-
-    total_exported = phase1_count + phase2_count
-    if total_exported == len(base_ids):
-        print(f"Successfully exported all {total_exported} volumes")
-    else:
-        print(f"Only exported {total_exported} volumes...")
+    get_xml_files(eebo_path, base_ids, output_path)
