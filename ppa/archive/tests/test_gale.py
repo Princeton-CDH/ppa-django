b8513ef932b9bc4d2d5d8b13f9a3835bd11806e4
Laure Thompson <602628+laurejt@users.noreply.github.com>
2024-09-19 15:57:39 -0400

    Fixed return value error for mocked method (resetting was not enough)

--- ppa/archive/tests/test_gale.py
+++ ppa/archive/tests/test_gale.py
@@ -285,6 +285,7 @@ class TestGaleAPI(TestCase):
         # skip apip call if record is provided
         mock_get_item.reset_mock()
         mock_get_local_ocr.reset_mock()
+        mock_get_local_ocr.side_effect = [FileNotFoundError, FileNotFoundError, "local ocr text"]
         page_data = list(gale_api.get_item_pages(item_id, api_response))
         mcok_get_item.assert_not_called()
         assert mock_get_local_ocr.call_count == 3
