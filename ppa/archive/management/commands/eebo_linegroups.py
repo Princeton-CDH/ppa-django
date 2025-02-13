e86b297c24f61e3c988862dd58d0c3fd3713c362
rlskoeser <rebecca.s.koeser@princeton.edu>
2025-02-13 14:33:08 -0500

    Refactor script to work on both eebo and ecco tcp files
    (WIP from December 2024)

--- ppa/archive/management/commands/eebo_linegroups.py
+++ ppa/archive/management/commands/eebo_linegroups.py
@@ -1,8 +1,9 @@
-from pathlib import Path
+import pathlib
 import csv
 
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
+from eulxml import xmlmap
 
 from ppa.archive import eebo_tcp
 from ppa.archive.models import DigitizedWork
@@ -29,7 +30,7 @@ class Command(BaseCommand):
             raise CommandError(
                 "Path for EEBO_DATA must be configured in Django settings"
             )
-        self.eebo_data_path = Path(settings.EEBO_DATA)
+        self.eebo_data_path = pathlib.Path(settings.EEBO_DATA)
         if not self.eebo_data_path.exists():
             raise CommandError(
                 f"EEBO_DATA directory {self.eebo_data_path} does not exist"
@@ -39,8 +40,17 @@ class Command(BaseCommand):
         digworks = DigitizedWork.objects.filter(
             status=DigitizedWork.PUBLIC, source=DigitizedWork.EEBO
         )
+        self.stdout.write(f"Found {digworks.count()} EEBO-TCP works")
 
-        with open("eebo-linegroups.csv", "w", encoding="utf-8-sig") as csvfile:
+        ecco_digworks = []
+        if hasattr(settings, "ECCO_TCP_DATA"):
+            self.ecco_tcp_path = pathlib.Path(settings.ECCO_TCP_DATA)
+            ecco_tcp_ids = [p.stem for p in self.ecco_tcp_path.glob("*.xml")]
+            self.stdout.write(f"Found {len(ecco_tcp_ids)} ECCO-TCP xml files")
+
+            ecco_digworks = DigitizedWork.objects.filter(source_id__in=ecco_tcp_ids)
+
+        with open("tcp-linegroups.csv", "w", encoding="utf-8-sig") as csvfile:
             csvwriter = csv.DictWriter(
                 csvfile,
                 fieldnames=[
@@ -59,37 +69,54 @@ class Command(BaseCommand):
             )
             csvwriter.writeheader()
 
+            # eebo-tcp works
             for work in digworks:
                 tcp_text = eebo_tcp.load_tcp_text(work.source_id)
-                for lg in tcp_text.line_groups:
-                    # TODO: PB REF != page number necessarily...
-                    # get based on index?
+                csvwriter.writerows(self.linegroups_for_text(work, tcp_text))
 
-                    # check if work is an excerpt
-                    if work.item_type != DigitizedWork.FULL:
-                        # if line groups start page is not in page span, skip
-                        if int(lg.start_page.index) not in work.page_span:
-                            continue
+            # ecco-tcp works
+            for work in ecco_digworks:
+                tcp_text = xmlmap.load_xmlobject_from_file(
+                    self.ecco_tcp_path / f"{work.source_id}.xml", eebo_tcp.Text
+                )
+                csvwriter.writerows(self.linegroups_for_text(work, tcp_text))
 
-                    # otherwise, add line group details to the CSV
-                    linegroup_info = {
-                        "source_id": work.source_id,
-                        "source_title": work.title,
-                        # "source_excerpt": "N"
-                        # if work.item_type == DigitizedWork.FULL
-                        # else "Y",
-                        # "excerpt_digital_pages": work.pages_digital,
-                        "start_page_index": lg.start_page.index,
-                        "end_page_index": lg.continue_page.index
-                        if lg.continue_page
-                        else None,
-                        "start_page_n": lg.start_page.number,
-                        "end_page_n": lg.continue_page.number
-                        if lg.continue_page
-                        else None,
-                        # "text": str(lg),
-                        "text": lg.text,
-                        "source_note": lg.source,
-                        "language": lg.language,
-                    }
-                    csvwriter.writerow(linegroup_info)
+    def linegroups_for_text(self, work, tcp_text):
+        # given a DigitizedWork and a TCP text, return a list of
+        # dictionaries with line groups found
+        linegroups = []
+        for lg in tcp_text.line_groups:
+            # TODO: PB REF != page number necessarily...
+            # get based on index?
+            # TODO: can we do better on page numbers?
+
+            # TODO: skip if it's all gap / foreign language / no text
+
+            # if work is an excerpt, only include line groups in range
+            if work.item_type != DigitizedWork.FULL:
+                # if line groups start page is not in page span, skip
+                if int(lg.start_page.index) not in work.page_span:
+                    continue
+
+            # otherwise, add line group details to the CSV
+            linegroups.append(
+                {
+                    "source_id": work.source_id,
+                    "source_title": work.title,
+                    # "source_excerpt": "N"
+                    # if work.item_type == DigitizedWork.FULL
+                    # else "Y",
+                    # "excerpt_digital_pages": work.pages_digital,
+                    "start_page_index": lg.start_page.index,
+                    "end_page_index": lg.continue_page.index
+                    if lg.continue_page
+                    else None,
+                    "start_page_n": lg.start_page.number,
+                    "end_page_n": lg.continue_page.number if lg.continue_page else None,
+                    # "text": str(lg),
+                    "text": lg.text,
+                    "source_note": lg.source,
+                    "language": lg.language,
+                }
+            )
+        return linegroups
