b03f958b66220916c12d3d6e52c2967a9a6301b6
Ben Silverman <ben@performantsoftware.com>
2025-05-19 16:52:14 -0400

    Add unit tests for PDF related functions (#559)

--- ppa/editorial/wagtail_hooks.py
+++ ppa/editorial/wagtail_hooks.py
@@ -6,4 +6,6 @@ from webpack_loader.templatetags.webpack_loader import render_bundle
 
 @hooks.register("insert_editor_js")
 def editor_js():
+    """Wagtail hook to include a JS bundle in the page editor, in html script
+    tags."""
     return format_html(render_bundle({}, "pdf", "js"))
